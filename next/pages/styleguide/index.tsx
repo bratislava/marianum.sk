3b5588867a0014ae4be997889b1dbc38f2fd00b4
Marek Bodinger <marek.bodinger@gmail.com>
2022-07-28 22:54:38 +0200

    MD-21 Add QuantitySelect to style guide

--- next/pages/styleguide/index.tsx
+++ next/pages/styleguide/index.tsx
@@ -1,5 +1,5 @@
 import cx from 'classnames'
-import React, { useMemo } from 'react'
+import React, { useMemo, useState } from 'react'
 
 import AddIcon from '../../assets/add.svg'
 import ArrowRightIcon from '../../assets/arrow_forward.svg'
@@ -17,6 +17,7 @@ import Accordion from '../../components/molecules/Accordion/Accordion'
 import AccordionItem from '../../components/molecules/Accordion/AccordionItem'
 import Row from '../../components/molecules/Row'
 import Search from '../../components/molecules/Search'
+import QuantitySelect from '../../components/atoms/QuantitySelect'
 
 interface IWrapperProps {
   title?: string
@@ -57,6 +58,8 @@ export const Stack = ({ bg, width = null, direction = 'row', children }: IStackP
 }
 
 const Showcase = () => {
+  const [quantitySelectValue, setQuantitySelectValue] = useState(1)
+
   const dummyBreadcrumbLinks = useMemo(
     () => [
       {
@@ -694,6 +697,17 @@ const Showcase = () => {
                 <p>09:00 - 18:00</p>
               </div>
             }
+          />
+        </Stack>
+      </Wrapper>
+
+      <Wrapper title="Quantity select">
+        <Stack>
+          <QuantitySelect
+            minValue={0}
+            maxValue={10}
+            value={quantitySelectValue}
+            onChange={(value) => setQuantitySelectValue(value)}
           />
         </Stack>
       </Wrapper>
