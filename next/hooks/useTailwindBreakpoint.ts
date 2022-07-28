1579f56a59d6922083cb5eb135658a584d6fd011
Matej Hospodár <matej.hospodar@bratislava.sk>
2022-07-28 13:23:21 +0200

    add comments to describe useTailwindBreakpoint

--- next/hooks/useTailwindBreakpoint.ts
+++ next/hooks/useTailwindBreakpoint.ts
@@ -1,5 +1,6 @@
 import { useCallback, useEffect, useState } from 'react'
 
+// todo: take this from some single source of truth
 const screens = {
   sm: 640,
   md: 768,
@@ -8,6 +9,8 @@ const screens = {
   '2xl': 1536,
 }
 
+// this hook returns current tailwind breakpoint ("sm" | "md" | "lg" | "xl" | "2xl")
+// or null when screen is smaller then "sm" breakpoint
 export const useTailwindBreakpoint = () => {
   const [screenWidth, setScreenWidth] = useState(0)
   const [breakpoint, setBreakpoint] = useState<keyof typeof screens | null>(null)
@@ -28,6 +31,7 @@ export const useTailwindBreakpoint = () => {
     let currentBreakpoint: keyof typeof screens | null = null
     let biggestBreakpointValue = 0
 
+    // find the biggest breakpoint
     ;(Object.keys(screens) as (keyof typeof screens)[]).forEach((bp) => {
       const breakpointValue = screens[bp]
       if (breakpointValue > biggestBreakpointValue && screenWidth >= breakpointValue) {
