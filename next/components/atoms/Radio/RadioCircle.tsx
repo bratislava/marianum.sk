8efe7734640ffc7db2365169fa17b5a65b53d1f6
Marek Bodinger <marek.bodinger@gmail.com>
2022-08-03 23:40:49 +0200

    Create RadioCircle.tsx

---
+++ next/components/atoms/Radio/RadioCircle.tsx
@@ -0,0 +1,32 @@
+import cx from 'classnames'
+
+const RadioCircle = ({
+  checked,
+  disabled,
+  active,
+}: {
+  checked: boolean
+  disabled: boolean
+  active: boolean
+}) => {
+  return (
+    /* It's recommended to add `group` classname to parent `RadioGroup.Option` for hover to work properly. */
+    <div
+      className={cx(
+        'grid place-content-center w-5 h-5 border border-2 rounded-full group-hover:border-primary-dark hover:border-primary-dark',
+        {
+          'opacity-50': disabled,
+          'border-primary': !active,
+          'border-primary-dark': active,
+          'bg-white': !checked,
+        },
+      )}
+    >
+      {checked && (
+        <div className={cx('w-3 h-3 rounded-full bg-primary group-hover:bg-primary-dark')} />
+      )}
+    </div>
+  )
+}
+
+export default RadioCircle
