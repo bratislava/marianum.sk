7510b02eec48ed9fd49ca59936082c645becf172
Matej Hospodár <matej.hospodar@bratislava.sk>
2022-08-03 12:55:43 +0200

    fix breakpoints

--- next/components/molecules/Navigation/Navigation.tsx
+++ next/components/molecules/Navigation/Navigation.tsx
@@ -3,19 +3,19 @@ import MarianumLogo from '../../../assets/marianum_logo.svg'
 import MenuIcon from '../../../assets/menu.svg'
 import PhoneIcon from '../../../assets/phone.svg'
 import MLink from '../../atoms/MLink'
-import NavigationMenu, { NavigationMenuProps } from './NavigationMenu'
+import NavigationMenuDesktop, { NavigationMenuDesktopProps } from './NavigationMenuDesktop'
 import NavigationSearch from './NavigationSearch'
 
 type NavigationProps = {
   phoneNumber?: string
   faqLink?: string
-  navigationItems: NavigationMenuProps['items']
+  navigationItems: NavigationMenuDesktopProps['items']
 }
 
 const Navigation = ({ phoneNumber, faqLink, navigationItems }: NavigationProps) => {
   return (
     <div className="bg-primary text-white">
-      <div className="container relative mx-auto flex h-16 items-center justify-between px-4 md:h-[120px] md:pb-8">
+      <div className="container relative mx-auto flex h-16 items-center justify-between px-4 lg:h-[120px] lg:pb-8">
         <MarianumLogo className="w-[108px] md:w-[142px]" />
         <div className="flex items-center gap-4 lg:gap-8">
           <div className="hidden items-center gap-8 xl:flex">
@@ -33,15 +33,16 @@ const Navigation = ({ phoneNumber, faqLink, navigationItems }: NavigationProps) 
             )}
           </div>
           <NavigationSearch />
+          {/* mobile menu button */}
           <button
             type="button"
-            className="flex h-10 w-10 items-center justify-center rounded-full bg-primary-dark md:hidden"
+            className="flex h-10 w-10 items-center justify-center rounded-full bg-primary-dark lg:hidden"
           >
             <MenuIcon width={24} height={24} />
           </button>
         </div>
-        <div className="absolute inset-x-0 -bottom-8 hidden px-4 md:block ">
-          <NavigationMenu items={navigationItems} />
+        <div className="absolute inset-x-0 -bottom-8 hidden px-4 lg:block ">
+          <NavigationMenuDesktop items={navigationItems} />
         </div>
       </div>
     </div>
