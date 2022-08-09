4bc7f020d64cf672d9a976640000807ae76af67a
Marek Bodinger <marek.bodinger@gmail.com>
2022-08-09 16:09:37 +0200

    test

--- next/components/atoms/Button.tsx
+++ next/components/atoms/Button.tsx
@@ -1,8 +1,14 @@
 /* eslint-disable sonarjs/no-duplicate-string */
 import cx from 'classnames'
+import { ButtonHTMLAttributes, DetailedHTMLProps, ForwardedRef, forwardRef } from 'react'
 
 import MLink from './MLink'
-import { ButtonHTMLAttributes, DetailedHTMLProps, ForwardedRef, forwardRef } from 'react'
+
+const removeGroupHover = (className: string) =>
+  className
+    .split(' ')
+    .filter((c) => !c.startsWith('group-hover:'))
+    .join(' ')
 
 type ButtonProps = DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> &
   React.AnchorHTMLAttributes<HTMLAnchorElement> & {
@@ -16,14 +22,24 @@ type ButtonProps = DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HT
       | 'plain-primary'
       | 'plain-secondary'
       | 'plain-white'
+    groupHover?: boolean
   }
 
 const Button = forwardRef<HTMLButtonElement | HTMLAnchorElement, ButtonProps>(
   (
-    { startIcon = null, endIcon = null, children, variant = 'primary', href, className, ...rest },
+    {
+      startIcon = null,
+      endIcon = null,
+      children,
+      variant = 'primary',
+      href,
+      className,
+      groupHover = false,
+      ...rest
+    },
     ref,
   ) => {
-    const style = cx(
+    const styleWithGroupHover = cx(
       'text-btn font-bold inline-flex items-center justify-center text-center align-middle space-x-2',
       className,
       {
@@ -37,22 +53,27 @@ const Button = forwardRef<HTMLButtonElement | HTMLAnchorElement, ButtonProps>(
 
         // text color
         'text-white': variant === 'primary' || variant === 'plain-white',
-        'text-primary hover:text-primary-dark':
+        'text-primary hover:text-primary-dark group-hover:text-primary-dark':
           variant === 'secondary' ||
           variant === 'tertiary' ||
           variant === 'white' ||
           variant === 'plain-primary',
-        'text-foreground hover:text-[#323532]': variant === 'plain-secondary',
-        'hover:opacity-64': variant === 'plain-white',
+        'text-foreground hover:text-[#323532] group-hover:text-[#323532]':
+          variant === 'plain-secondary',
+        'hover:opacity-64 group-hover:opacity-64': variant === 'plain-white',
 
         // bg and border color
-        'bg-primary border border-primary hover:bg-primary-dark hover:border-primary-dark':
+        'bg-primary border border-primary hover:bg-primary-dark group-hover:bg-primary-dark hover:border-primary-dark group-hover:border-primary-dark':
           variant === 'primary',
-        'border border-primary hover:border-primary-dark': variant === 'secondary',
-        'border border-border-alt hover:border-border-alt-dark': variant === 'tertiary',
+        'border border-primary hover:border-primary-dark group-hover:border-primary-dark':
+          variant === 'secondary',
+        'border border-border-alt hover:border-border-alt-dark group-hover:border-border-alt-dark':
+          variant === 'tertiary',
         'bg-white border border-white': variant === 'white',
       },
     )
+
+    const style = groupHover ? styleWithGroupHover : removeGroupHover(styleWithGroupHover)
 
     if (href) {
       return (
