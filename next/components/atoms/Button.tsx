2a0791185599be4b3535d4f8bf946c115aca375a
MarekBodingerBA <104828482+MarekBodingerBA@users.noreply.github.com>
2022-08-11 22:05:51 +0200

    MD-59 Add forwardRef with polymorphic props to button (#26)
    * MD-59 Add forwardRef with polymorphic props to button
    
    * Omit LinkButtonProps from ButtonProps
    
    Co-authored-by: Marek Bodinger <marek.bodinger@gmail.com>

--- next/components/atoms/Button.tsx
+++ next/components/atoms/Button.tsx
@@ -1,11 +1,13 @@
 /* eslint-disable sonarjs/no-duplicate-string */
+import { LinkButtonProps } from '@react-types/button'
 import classnames from 'classnames'
-import { ReactNode, useRef } from 'react'
+import { forwardRef, ReactNode, Ref, RefObject } from 'react'
+import * as React from 'react'
 import { AriaButtonProps, useButton } from 'react-aria'
 
 import MLink from './MLink'
 
-type ButtonProps = (AriaButtonProps<'button'> | AriaButtonProps<'a'>) & {
+type ButtonBase = {
   startIcon?: ReactNode
   endIcon?: ReactNode
   variant?:
@@ -21,99 +23,128 @@ type ButtonProps = (AriaButtonProps<'button'> | AriaButtonProps<'a'>) & {
   tabIndex?: number
 }
 
-const Button = ({
-  startIcon = null,
-  endIcon = null,
-  variant = 'primary',
-  className,
-  children,
-  disabled = false,
-  tabIndex = 0,
-  ...rest
-}: ButtonProps) => {
-  const ref = useRef<HTMLButtonElement>(null)
-  const { buttonProps } = useButton(
+/*
+ *  This part makes the component return `HTMLAnchorElement` ref when `href` if provided and `HTMLButtonElement` when it's not.
+ *  https://github.com/typescript-cheatsheets/react/issues/167#issuecomment-751347673
+ */
+type ButtonProps = Omit<AriaButtonProps<'button'>, keyof LinkButtonProps> &
+  ButtonBase & { ref?: Ref<HTMLButtonElement>; href?: undefined }
+type AnchorProps = AriaButtonProps<'a'> &
+  ButtonBase & {
+    ref?: Ref<HTMLAnchorElement>
+    href: string
+  }
+
+type PolymorphicProps = ButtonProps | AnchorProps
+type PolymorphicButton = {
+  (props: AnchorProps): JSX.Element
+  (props: ButtonProps): JSX.Element
+}
+
+const Button = forwardRef<HTMLAnchorElement | HTMLButtonElement, PolymorphicProps>(
+  (
     {
-      ...rest,
-      elementType: rest.href ? 'a' : 'button',
-      isDisabled: disabled,
+      startIcon = null,
+      endIcon = null,
+      variant = 'primary',
+      className,
+      children,
+      disabled = false,
+      tabIndex = 0,
+      ...rest
     },
     ref,
-  )
+  ) => {
+    const { buttonProps } = useButton(
+      {
+        ...rest,
+        elementType: rest.href ? 'a' : 'button',
+        isDisabled: disabled,
+      },
+      ref as RefObject<HTMLAnchorElement | HTMLButtonElement>,
+    )
 
-  const style = classnames(
-    'inline-flex items-center justify-center space-x-2 text-center align-middle text-btn font-bold focus:outline-none',
-    className,
-    {
-      'px-6 py-2':
-        variant === 'primary' ||
-        variant === 'secondary' ||
-        variant === 'tertiary' ||
-        variant === 'white',
-      'rounded px-2':
-        variant === 'plain-primary' || variant === 'plain-secondary' || variant === 'plain-white',
+    const style = classnames(
+      'inline-flex items-center justify-center space-x-2 text-center align-middle text-btn font-bold focus:outline-none',
+      className,
+      {
+        'px-6 py-2':
+          variant === 'primary' ||
+          variant === 'secondary' ||
+          variant === 'tertiary' ||
+          variant === 'white',
+        'rounded px-2':
+          variant === 'plain-primary' || variant === 'plain-secondary' || variant === 'plain-white',
 
-      // text color
-      'text-white': variant === 'primary' || variant === 'plain-white',
-      'text-primary':
-        variant === 'secondary' ||
-        variant === 'tertiary' ||
-        variant === 'white' ||
-        variant === 'plain-primary',
-      'text-foreground': variant === 'plain-secondary',
+        // text color
+        'text-white': variant === 'primary' || variant === 'plain-white',
+        'text-primary':
+          variant === 'secondary' ||
+          variant === 'tertiary' ||
+          variant === 'white' ||
+          variant === 'plain-primary',
+        'text-foreground': variant === 'plain-secondary',
 
-      // bg and border color
-      'border border-primary bg-primary focus:border-primary-dark focus:bg-primary-light':
-        variant === 'primary',
-      'border border-primary focus:border-primary-light focus:text-primary-light':
-        variant === 'secondary',
-      'border border-border-alt focus:text-primary-light': variant === 'tertiary',
-      'border border-white bg-white focus:border-primary-dark focus:text-primary-light':
-        variant === 'white',
-      'focus:bg-primary/8 focus:text-primary-light': variant === 'plain-primary',
-      'focus:bg-foreground/8 focus:text-foreground': variant === 'plain-secondary',
-      'hover:bg-white/12 focus:bg-white/8': variant === 'plain-white',
+        // bg and border color
+        'border border-primary bg-primary focus:border-primary-dark focus:bg-primary-light':
+          variant === 'primary',
+        'border border-primary focus:border-primary-light focus:text-primary-light':
+          variant === 'secondary',
+        'border border-border-alt focus:text-primary-light': variant === 'tertiary',
+        'border border-white bg-white focus:border-primary-dark focus:text-primary-light':
+          variant === 'white',
+        'focus:bg-primary/8 focus:text-primary-light': variant === 'plain-primary',
+        'focus:bg-foreground/8 focus:text-foreground': variant === 'plain-secondary',
+        'hover:bg-white/12 focus:bg-white/8': variant === 'plain-white',
 
-      // hover
-      'hover:border-primary-dark hover:bg-primary-dark': variant === 'primary' && !disabled,
-      'border border-primary hover:border-primary-dark hover:text-primary-dark':
-        variant === 'secondary' && !disabled,
-      'hover:border-primary-dark hover:text-primary-dark': variant === 'tertiary' && !disabled,
-      'hover:border-border-alt-dark hover:text-primary-dark': variant === 'white' && !disabled,
-      'hover:bg-primary/12 hover:text-primary-dark': variant === 'plain-primary' && !disabled,
-      'hover:bg-foreground/12 hover:text-foreground-dark':
-        variant === 'plain-secondary' && !disabled,
-      'hover:opacity-64': variant === 'plain-white' && !disabled,
+        // hover
+        'hover:border-primary-dark hover:bg-primary-dark': variant === 'primary' && !disabled,
+        'border border-primary hover:border-primary-dark hover:text-primary-dark':
+          variant === 'secondary' && !disabled,
+        'hover:border-primary-dark hover:text-primary-dark': variant === 'tertiary' && !disabled,
+        'hover:border-border-alt-dark hover:text-primary-dark': variant === 'white' && !disabled,
+        'hover:bg-primary/12 hover:text-primary-dark': variant === 'plain-primary' && !disabled,
+        'hover:bg-foreground/12 hover:text-foreground-dark':
+          variant === 'plain-secondary' && !disabled,
+        'hover:opacity-64': variant === 'plain-white' && !disabled,
 
-      // disabled
-      'opacity-50': disabled,
-    },
-  )
+        // disabled
+        'opacity-50': disabled,
+      },
+    )
 
-  if (rest.href) {
+    if (rest.href) {
+      return (
+        <MLink
+          ref={ref as RefObject<HTMLAnchorElement>}
+          href={rest.href}
+          tabIndex={tabIndex}
+          noArrow
+          noStyles
+          className={style}
+          {...buttonProps}
+        >
+          {startIcon}
+          <span>{children}</span>
+          {endIcon}
+        </MLink>
+      )
+    }
+
     return (
-      <MLink
-        href={rest.href}
+      <button
+        type="button"
+        ref={ref as RefObject<HTMLButtonElement>}
         tabIndex={tabIndex}
-        noArrow
-        noStyles
         className={style}
         {...buttonProps}
       >
         {startIcon}
         <span>{children}</span>
         {endIcon}
-      </MLink>
+      </button>
     )
-  }
-
-  return (
-    <button type="button" ref={ref} tabIndex={tabIndex} className={style} {...buttonProps}>
-      {startIcon}
-      <span>{children}</span>
-      {endIcon}
-    </button>
-  )
-}
+  },
+) as PolymorphicButton
 
 export default Button
