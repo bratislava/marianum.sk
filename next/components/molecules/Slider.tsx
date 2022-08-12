9f80cb876d850c5d7e5ae99f729996deae981137
Matej Hospodár <matej.hospodar@bratislava.sk>
2022-08-12 10:21:41 +0200

    MD-60 Slider component (#19)
    * add framer-motion library
    
    * add AnimateHeight component
    
    * add Checklist component
    
    * hide title checkmark on desktop
    
    * hide some files
    
    * add reduced motion support
    
    * add shadow, reenable containers
    
    * add marianum logo
    
    * remove width and height so it can be styled from tailwind
    
    * extend props with inputClassName
    
    * extend props with classNames
    
    * add simple Navigation
    
    * remove unused span
    
    * add phone number and faq
    
    * add radix dropdown menu and tailwind plugin for handling radix state
    
    * add className prop
    
    * add Menu and SubMenu components
    
    * add navigation items
    
    * add looping
    
    * remove FC as children
    
    * add dividers between navigation items
    
    * retrieve render props
    
    * fix focus opacity
    
    * add dark bg
    
    * animate chevron with framer
    
    * replace chevron_right with chevron_down
    
    * use MLinks, close menu on route change
    
    * fix breakpoints
    
    * rename NavigationMenu to NavigationMenuDesktop
    
    * add IconButton
    
    * add mobile navigation
    
    * add popmotion package
    
    * fix navigation
    
    * add Slider and HomepageSlider
    
    * add z-index to navigation
    
    * add pagination
    
    * extract Slide type
    
    * set description opacity to 72
    
    * fix spacing
    
    * Merge branch 'master' of github.com:bratislava/marianum into feature/MD-60/slider-component

---
+++ next/components/molecules/Slider.tsx
@@ -0,0 +1,134 @@
+import { AnimatePresence, motion, PanInfo, Variant } from 'framer-motion'
+import { wrap } from 'popmotion'
+import { FC, ReactNode, useCallback, useEffect, useState } from 'react'
+
+type SliderProps = {
+  pages: ReactNode[]
+  autoSwipeDuration?: number
+  pagination?: FC<{
+    count: number
+    activeIndex: number
+    goToPage: (index: number) => void
+    goToPrevious: () => void
+    goToNext: () => void
+  }>
+}
+
+const variants: { [name: string]: Variant } = {
+  enter: (direction: number) => {
+    return {
+      x: direction > 0 ? 1000 : -1000,
+      opacity: 0,
+    }
+  },
+  center: {
+    zIndex: 1,
+    x: 0,
+    opacity: 1,
+  },
+  exit: (direction: number) => {
+    return {
+      zIndex: 0,
+      x: direction < 0 ? 1000 : -1000,
+      opacity: 0,
+    }
+  },
+}
+
+const swipeConfidenceThreshold = 10_000
+
+const swipePower = (offset: number, velocity: number) => {
+  return Math.abs(offset) * velocity
+}
+
+const Slider = ({ pages, autoSwipeDuration = 0, pagination: Pagination }: SliderProps) => {
+  const [[page, direction], setPage] = useState([0, 0])
+  const [isDragging, setDragging] = useState(false)
+  const index = wrap(0, pages.length, page)
+
+  const paginate = useCallback((newDirection: number) => {
+    setPage(([currentPage]) => [currentPage + newDirection, newDirection])
+  }, [])
+
+  const goToNext = useCallback(() => {
+    paginate(1)
+  }, [paginate])
+
+  const goToPrevious = useCallback(() => {
+    paginate(-1)
+  }, [paginate])
+
+  const dragEndHandler = useCallback(
+    (event: MouseEvent | TouchEvent | PointerEvent, { offset, velocity }: PanInfo) => {
+      const swipe = swipePower(offset.x, velocity.x)
+
+      if (swipe < -swipeConfidenceThreshold) {
+        goToNext()
+      } else if (swipe > swipeConfidenceThreshold) {
+        goToPrevious()
+      }
+      setDragging(false)
+    },
+    [goToNext, goToPrevious],
+  )
+
+  const dragStartHandler = useCallback(() => {
+    setDragging(true)
+  }, [])
+
+  useEffect(() => {
+    if (!autoSwipeDuration || isDragging) return () => {}
+
+    const timer = setInterval(() => {
+      paginate(1)
+    }, autoSwipeDuration)
+
+    return () => clearInterval(timer)
+  }, [autoSwipeDuration, paginate, isDragging])
+
+  const goToPage = useCallback(
+    (goToIndex: number) => {
+      const newDirection = index < goToIndex ? 1 : -1
+      setPage([goToIndex, newDirection])
+    },
+    [index],
+  )
+
+  return (
+    <div className="relative z-0 flex h-full w-full items-center justify-center overflow-hidden">
+      <AnimatePresence initial={false} custom={direction}>
+        <motion.div
+          className="absolute h-full w-full"
+          key={page}
+          custom={direction}
+          variants={variants}
+          initial="enter"
+          animate="center"
+          exit="exit"
+          transition={{
+            x: { type: 'spring', stiffness: 300, damping: 30 },
+            opacity: { duration: 0.2 },
+          }}
+          drag="x"
+          dragConstraints={{ left: 0, right: 0 }}
+          dragElastic={1}
+          onDragStart={dragStartHandler}
+          onDragEnd={dragEndHandler}
+        >
+          {pages[index]}
+        </motion.div>
+      </AnimatePresence>
+      {Pagination && (
+        <Pagination
+          count={pages.length}
+          activeIndex={index}
+          goToPage={goToPage}
+          goToNext={goToNext}
+          goToPrevious={goToPrevious}
+        />
+      )}
+    </div>
+  )
+}
+
+export default Slider
--- next/pages/index.tsx
+++ next/pages/index.tsx
@@ -1,8 +1,10 @@
+/* eslint-disable sonarjs/no-duplicate-string */
 import { GetStaticProps } from 'next'
 import { useTranslation } from 'next-i18next'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 
 import PageWrapper from '../components/layouts/PageWrapper'
+import HomepageSlider from '../components/sections/HomepageSlider'
 import { NavigationItemFragment } from '../graphql'
 import { client } from '../utils/gql'
 
@@ -17,7 +19,35 @@ const Home = ({ navigation, faqLink, phoneNumber }: HomeProps) => {
 
   return (
     <PageWrapper navigation={navigation} faqLink={faqLink} phoneNumber={phoneNumber}>
-      {t('general.hello')}
+      <HomepageSlider
+        slides={[
+          {
+            key: 'slide-1',
+            title: 'Slide 1',
+            description:
+              'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.',
+            imageSrc: '',
+            buttonText: 'Zistiť viac',
+          },
+          {
+            key: 'slide-2',
+            title: 'Slide 2',
+            description:
+              'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.',
+            imageSrc: '',
+            buttonText: 'Zistiť viac',
+          },
+          {
+            key: 'slide-3',
+            title: 'Slide 3',
+            description:
+              'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.',
+            imageSrc: '',
+            buttonText: 'Zistiť viac',
+          },
+        ]}
+      />
+      <div>{t('general.hello')}</div>
     </PageWrapper>
   )
 }
