de5ef353604cf7749f455b44bf43a0d98a084c4d
Radoslav Zeman <radoslav.zeman@bratislava.sk>
2022-08-12 08:15:26 +0200

    update cms structure

--- next/components/sections/HeroSection.tsx
+++ next/components/sections/HeroSection.tsx
@@ -1,22 +1,23 @@
-import React from 'react'
+import Image from 'next/image'
 
 import HomeIcon from '../../assets/home.svg'
-import { CtaButtonFragment } from '../../graphql'
+import { CtaButtonFragment, UploadImageEntityFragment } from '../../graphql'
 import Breadcrumbs from '../atoms/Breadcrumbs'
 import Button from '../atoms/Button'
 import MLink from '../atoms/MLink'
 
 type HeroSectionProps = {
   title?: string | null | undefined
-  description?: string | null | undefined
+  perex?: string | null | undefined
   cta?: CtaButtonFragment | null | undefined
+  image?: UploadImageEntityFragment | null | undefined
 }
 
-const HeroSection = ({ title, description, cta }: HeroSectionProps) => {
+const HeroSection = ({ title, perex, cta, image }: HeroSectionProps) => {
   return (
     <div className="bg-primary-dark text-white/72">
       <div className="container relative mx-auto px-4">
-        <Breadcrumbs className="md:pt-8">
+        <Breadcrumbs className="sm:pt-8">
           {[
             {
               label: <HomeIcon />,
@@ -32,9 +33,9 @@ const HeroSection = ({ title, description, cta }: HeroSectionProps) => {
             </MLink>
           ))}
         </Breadcrumbs>
-        <div className="py-5 text-white empty:hidden md:w-[648px] md:pb-14 md:pt-6">
-          {title && <h1>{title}</h1>}
-          {description && <p className="mt-3 text-white opacity-72">{description}</p>}
+        <div className="py-5 empty:hidden md:w-[648px] md:pb-14 md:pt-6">
+          {title && <h1 className="text-white">{title}</h1>}
+          {perex && <p className="mt-3">{perex}</p>}
           {cta && (
             <Button href={cta.url} className="mt-6">
               {cta.label}
@@ -42,6 +43,17 @@ const HeroSection = ({ title, description, cta }: HeroSectionProps) => {
           )}
         </div>
       </div>
+      {image && (
+        <div className="container relative mx-auto -mb-18 h-[188px] w-full sm:h-[238px] md:h-[287px] lg:h-[387px] xl:h-[440px]">
+          <Image
+            src={image.attributes?.url ?? ''}
+            alt={image.attributes?.alternativeText ?? ''}
+            layout="fill"
+            objectFit="cover"
+            unoptimized
+          />
+        </div>
+      )}
     </div>
   )
 }
--- strapi/src/api/tmp/controllers/tmp.ts
+++
@@ -1,7 +0,0 @@
-/**
- *  tmp controller
- */
-
-import { factories } from '@strapi/strapi'
-
-export default factories.createCoreController('api::tmp.tmp');
