ca0f15df6365079dc43f3e41174e3006c6284e0d
Matej Hospodár <matej.hospodar@bratislava.sk>
2022-09-04 21:41:55 +0200

    display numbers aside

--- next/components/sections/ContactsSection.tsx
+++ next/components/sections/ContactsSection.tsx
@@ -5,6 +5,7 @@ import PhoneIcon from '../../assets/phone.svg'
 import { ContactGroupFragment } from '../../graphql'
 import { isDefined } from '../../utils/isDefined'
 import MLink from '../atoms/MLink'
+import Section from '../molecules/Section'
 
 const ContactsSection = ({ contacts }: ContactGroupFragment) => {
   const filteredContacts = useMemo(() => {
@@ -12,49 +13,55 @@ const ContactsSection = ({ contacts }: ContactGroupFragment) => {
   }, [contacts])
 
   return (
-    <div className="container mx-auto flex flex-col gap-4 px-4 py-8">
-      {filteredContacts.map(({ title, email, phone1, phone2 }) => (
-        <div className="border border-border bg-white">
-          <div className="p-6 text-h4 font-bold">{title}</div>
-          <hr className="border-border" />
-          <div className="flex flex-col gap-2 p-6">
-            {phone1 && (
-              <MLink
-                noStyles
-                // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
-                href={`tel:${phone1}`}
-                className="flex items-center gap-x-2 font-semibold text-primary"
-              >
-                <PhoneIcon />
-                {phone1}
-              </MLink>
-            )}
-            {phone2 && (
-              <MLink
-                noStyles
-                // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
-                href={`tel:${phone2}`}
-                className="flex items-center gap-x-2 font-semibold text-primary"
-              >
-                <PhoneIcon />
-                {phone2}
-              </MLink>
-            )}
-            {email && (
-              <MLink
-                noStyles
-                // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
-                href={`mailto:${email}`}
-                className="flex items-center gap-x-2 font-semibold text-primary"
-              >
-                <MailIcon />
-                {email}
-              </MLink>
-            )}
+    <Section isContainer>
+      <div className="flex flex-col gap-4">
+        {filteredContacts.map(({ title, email, phone1, phone2 }) => (
+          <div className="border border-border bg-white">
+            <div className="p-6 text-h4 font-bold">{title}</div>
+            <hr className="border-border" />
+            <div className="flex flex-col gap-2 p-6">
+              {(phone1 ?? phone2) && (
+                <div className="flex items-center gap-x-2 font-semibold text-primary">
+                  <PhoneIcon />
+                  <div>
+                    {phone1 && (
+                      <MLink
+                        noStyles
+                        // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+                        href={`tel:${phone1}`}
+                      >
+                        {phone1}
+                      </MLink>
+                    )}
+                    <span>, </span>
+                    {phone2 && (
+                      <MLink
+                        noStyles
+                        // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+                        href={`tel:${phone2}`}
+                      >
+                        {phone2}
+                      </MLink>
+                    )}
+                  </div>
+                </div>
+              )}
+              {email && (
+                <MLink
+                  noStyles
+                  // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+                  href={`mailto:${email}`}
+                  className="flex items-center gap-x-2 font-semibold text-primary"
+                >
+                  <MailIcon />
+                  {email}
+                </MLink>
+              )}
+            </div>
           </div>
-        </div>
-      ))}
-    </div>
+        ))}
+      </div>
+    </Section>
   )
 }
 
