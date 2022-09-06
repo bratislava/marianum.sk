a493a6ad77ba98533fc04097768ba8cd15e3dbbe
Matej Hospodár <matej.hospodar@bratislava.sk>
2022-09-06 10:39:01 +0200

    add section wrapper and use its context in section

---
+++ next/components/layouts/SectionsWrapper.tsx
@@ -0,0 +1,89 @@
+import { createContext, HTMLProps, ReactNode, useCallback, useMemo } from 'react'
+
+export type BackgroundColor = 'dark' | 'light'
+
+type SectionContextValue = {
+  getBackground: (index: number) => BackgroundColor
+  getDivider: (index: number) => boolean
+  getLast: (index: number) => boolean
+}
+
+export const sectionContext = createContext<SectionContextValue>({
+  getBackground: () => 'light',
+  getDivider: () => false,
+  getLast: () => false,
+})
+
+export type SectionsWrapperProps = {
+  children: ReactNode
+  alternateBackground?: boolean
+  startBackground?: BackgroundColor
+  background?: BackgroundColor
+} & HTMLProps<HTMLDivElement>
+
+const SectionsWrapper = ({
+  children,
+  alternateBackground = false,
+  startBackground = 'light',
+  background = 'light',
+  ...rest
+}: SectionsWrapperProps) => {
+  const sectionCount = useMemo(() => (Array.isArray(children) ? children.length : 1), [children])
+
+  const oddBackground: BackgroundColor = useMemo(() => startBackground, [startBackground])
+
+  const evenBackground: BackgroundColor = useMemo(
+    () => (startBackground === 'light' ? 'dark' : 'light'),
+    [startBackground],
+  )
+
+  const getBackground = useCallback(
+    (index: number): BackgroundColor => {
+      const oddOrEven = !!((index + 1) % 2)
+      if (alternateBackground) {
+        // last section is always dark when alternating
+        if (sectionCount === index + 1) {
+          return 'dark'
+        }
+        // otherwise alternate
+        return oddOrEven ? evenBackground : oddBackground
+      }
+      return background
+    },
+    [oddBackground, evenBackground, alternateBackground, background, sectionCount],
+  )
+
+  const getDivider = useCallback(
+    (index: number) => {
+      // is alternating && is last && previous is dark => then divider is displayed
+      return (
+        alternateBackground && sectionCount === index + 1 && getBackground(index - 1) === 'dark'
+      )
+    },
+    [sectionCount, alternateBackground, getBackground],
+  )
+
+  const getLast = useCallback(
+    (index: number) => {
+      return sectionCount === index + 1
+    },
+    [sectionCount],
+  )
+
+  const contextValue: SectionContextValue = useMemo(
+    () => ({
+      getBackground,
+      getDivider,
+      getLast,
+    }),
+    [getBackground, getDivider, getLast],
+  )
+
+  return (
+    <sectionContext.Provider value={contextValue}>
+      <div {...rest}>{children}</div>
+    </sectionContext.Provider>
+  )
+}
+
+export default SectionsWrapper
