9ef4cd87c2edd79ce6ce0db8a1c619a96e65cc52
Radoslav Zeman <radoslav.zeman@bratislava.sk>
2022-09-06 09:44:39 +0200

    add document page and document section

---
+++ next/components/molecules/DocumentGroup.tsx
@@ -0,0 +1,43 @@
+import { useTranslation } from 'next-i18next'
+import { useMemo } from 'react'
+
+import DownloadIcon from '../../assets/download.svg'
+import { DocumentGroupFragment } from '../../graphql'
+import { isDefined } from '../../utils/isDefined'
+import Button from '../atoms/Button'
+import Row from './Row'
+
+const DocumentGroup = ({ documents }: DocumentGroupFragment) => {
+  const { t } = useTranslation()
+
+  const filteredDocuments = useMemo(() => {
+    return (documents ?? [])
+      .map((document) => document?.document?.data?.attributes)
+      .filter(isDefined)
+  }, [documents])
+  return (
+    <div className="flex flex-col gap-4">
+      {filteredDocuments?.map(({ title, slug, documentCategory, file }) => (
+        <Row
+          title={title}
+          category={documentCategory?.data}
+          // TODO add proper path
+          link={slug ? `/documents/${slug}` : '#'}
+          button={
+            file.data?.attributes?.url ? (
+              <Button
+                href={file.data?.attributes?.url}
+                variant="tertiary"
+                startIcon={<DownloadIcon />}
+              >
+                {t('layouts.DocumentLayout.downloadFile')}
+              </Button>
+            ) : null
+          }
+        />
+      ))}
+    </div>
+  )
+}
+
+export default DocumentGroup
