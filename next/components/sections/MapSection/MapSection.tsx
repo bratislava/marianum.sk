8e731632d5e0ddc7bf7e61bd0c32dfabea6d24a2
Matej Hospodár <matej.hospodar@bratislava.sk>
2022-09-07 11:49:17 +0200

    fit to bounds, mobile design

--- next/components/sections/MapSection/MapSection.tsx
+++ next/components/sections/MapSection/MapSection.tsx
@@ -1,23 +1,42 @@
+import cx from 'classnames'
 import { motion } from 'framer-motion'
 import { useTranslation } from 'next-i18next'
-import { Fragment, useMemo, useState } from 'react'
-import Map, { Marker } from 'react-map-gl'
+import { Fragment, useCallback, useMemo, useRef, useState } from 'react'
+import Map, { MapRef, Marker } from 'react-map-gl'
 import slugify from 'slugify'
 import useSWR from 'swr'
 
 import MapMarkerIcon from '../../../assets/map-marker.svg'
 import PlaceIcon from '../../../assets/place.svg'
+import { Enum_Branch_Cemeterytype, Enum_Branch_Type } from '../../../graphql'
 import { client } from '../../../utils/gql'
 import { isDefined } from '../../../utils/isDefined'
+import Button from '../../atoms/Button'
 import MLink from '../../atoms/MLink'
 import TagToggle from '../../atoms/TagToggle'
 import Search from '../../molecules/Search'
 import Section, { SectionProps } from '../../molecules/Section'
 
+const slugifyText = (text: string) => {
+  return slugify(text, { replacement: ' ', lower: true })
+}
+
+// calculate bounding box for branches
+const getBoundsForBranches = (
+  branches: { longitude?: number | null; latitude?: number | null }[],
+) => {
+  const branchesLongitude = branches.map((branch) => branch.longitude).filter(isDefined) ?? []
+  const branchesLatitude = branches.map((branch) => branch.latitude).filter(isDefined) ?? []
+  return [
+    [Math.min(...branchesLongitude), Math.min(...branchesLatitude)],
+    [Math.max(...branchesLongitude), Math.max(...branchesLatitude)],
+  ] as [[number, number], [number, number]]
+}
+
 type MapSectionProps = Pick<SectionProps, 'isContainer' | 'color' | 'title'>
 
 const MapSection = ({ ...rest }: MapSectionProps) => {
-  const { i18n } = useTranslation()
+  const { t, i18n } = useTranslation('common', { keyPrefix: 'sections.MapSection' })
 
   const { data, error } = useSWR(['Branches', i18n.language], (_key, locale) =>
     client.Branches({ locale }),
@@ -29,29 +48,60 @@ const MapSection = ({ ...rest }: MapSectionProps) => {
         ?.map((branch) => branch.attributes)
         .filter(isDefined)
         .filter(
-          (branch) => branch.address && branch.title && branch.latitude && branch.longitude,
-        ) ?? []
+          (branch) =>
+            branch.address &&
+            branch.title &&
+            branch.latitude &&
+            branch.longitude &&
+            branch.type === Enum_Branch_Type.Cintorin,
+        )
+        // sort by title alphabetically
+        .sort((a, b) => a.title.localeCompare(b.title)) ?? []
     )
   }, [data?.branches])
 
   const [searchQuery, setSearchQuery] = useState('')
 
   const slugifiedSearchQuery = useMemo(() => {
-    return slugify(searchQuery, { replacement: ' ', lower: true })
+    return slugifyText(searchQuery)
   }, [searchQuery])
+
+  const [hoveredBranchSlug, setHoveredBranchSlug] = useState<string | null>(null)
+
+  const [isCivilChecked, setCivilChecked] = useState(false)
+  const [isHistoricalChecked, setHistoricalChecked] = useState(false)
+  const [isWarChecked, setWarChecked] = useState(false)
 
   const filteredBranches = useMemo(() => {
     return validBranches.filter(
       (branch) =>
-        (slugify(branch.address ?? '', { replacement: ' ', lower: true }).includes(
-          slugifiedSearchQuery,
-        ) ||
-          slugify(branch.title, { replacement: ' ', lower: true }).includes(
-            slugifiedSearchQuery,
-          )) ??
+        // search filter for address and title
+        ((slugifyText(branch.address ?? '').includes(slugifiedSearchQuery) ||
+          slugifyText(branch.title).includes(slugifiedSearchQuery)) &&
+          // if no cemetery type selected, show all
+          ((!isCivilChecked && !isHistoricalChecked && !isWarChecked) ||
+            // otherwise
+            // civil cemetery filter
+            (isCivilChecked && branch.cemeteryType === Enum_Branch_Cemeterytype.Civilny) ||
+            // historical cemetery filter
+            (isHistoricalChecked && branch.cemeteryType === Enum_Branch_Cemeterytype.Historicky) ||
+            // war cemetery filter
+            (isWarChecked && branch.cemeteryType === Enum_Branch_Cemeterytype.Vojensky))) ??
         [],
     )
-  }, [validBranches, slugifiedSearchQuery])
+  }, [validBranches, slugifiedSearchQuery, isCivilChecked, isHistoricalChecked, isWarChecked])
+
+  const mapRef = useRef<MapRef | null>(null)
+
+  const fitBranches = useCallback(() => {
+    mapRef.current?.fitBounds(getBoundsForBranches(filteredBranches), {
+      padding: 100,
+      offset: [0, 10],
+      duration: 0,
+    })
+  }, [filteredBranches])
+
+  const [isMapOrFiltersDisplayed, setMapOrFiltersDisplayed] = useState(false)
 
   // TODO replace by proper loading and error
   if (!data && !error) {
@@ -64,28 +114,43 @@ const MapSection = ({ ...rest }: MapSectionProps) => {
 
   return (
     <Section {...rest}>
-      <div className="flex h-[624px]">
-        <div className="h-full w-[360px] bg-white">
-          <div className="flex flex-col gap-3 border-b border-border p-5">
-            <Search
-              value={searchQuery}
-              onChange={setSearchQuery}
-              placeholder="Search..."
-              // eslint-disable-next-line no-alert
-              onSearch={(value) => alert(`Searching for: ${value}`)}
-            />
+      <div className="relative flex h-[624px] flex-col items-center overflow-hidden md:flex-row md:items-stretch">
+        <div
+          className={cx(
+            'absolute z-20 flex h-full w-full flex-col bg-white transition-transform duration-500 md:relative md:w-[360px]',
+            {
+              '-translate-x-full md:translate-x-0': isMapOrFiltersDisplayed,
+            },
+          )}
+        >
+          {/* filtering */}
+          <div className="flex flex-col gap-3 border-b border-border  p-5">
+            <Search value={searchQuery} onChange={setSearchQuery} placeholder={t('search')} />
             <div className="flex gap-2">
-              <TagToggle>Civilné</TagToggle>
-              <TagToggle>Historické</TagToggle>
-              <TagToggle>Vojnové</TagToggle>
+              <TagToggle isSelected={isCivilChecked} onChange={setCivilChecked}>
+                {t('filters.civil')}
+              </TagToggle>
+              <TagToggle isSelected={isHistoricalChecked} onChange={setHistoricalChecked}>
+                {t('filters.historical')}
+              </TagToggle>
+              <TagToggle isSelected={isWarChecked} onChange={setWarChecked}>
+                {t('filters.war')}
+              </TagToggle>
             </div>
           </div>
-          <div className="py-1">
+          {/* results */}
+          <div className="flex-1 overflow-auto" onMouseLeave={() => setHoveredBranchSlug(null)}>
             {filteredBranches.map(({ title, address, slug }, index) => (
               <Fragment key={slug}>
                 {index !== 0 && <hr className="mx-5 border-border" />}
-                {/* eslint-disable-next-line @typescript-eslint/restrict-template-expressions */}
-                <MLink noStyles href={`/branches/${slug}`} className="flex gap-2 px-5 py-3">
+                <MLink
+                  onMouseEnter={() => setHoveredBranchSlug(slug)}
+                  noStyles
+                  href={`/branches/${slug}`}
+                  className={cx('flex gap-2 px-5 py-3', {
+                    'bg-primary/5': slug === hoveredBranchSlug,
+                  })}
+                >
                   <div className="pt-[2px] text-primary">
                     <PlaceIcon />
                   </div>
@@ -96,20 +161,18 @@ const MapSection = ({ ...rest }: MapSectionProps) => {
                 </MLink>
               </Fragment>
             ))}
+            {filteredBranches.length === 0 && <div className="p-5">{t('noResults')}</div>}
           </div>
         </div>
         <div className="h-full w-full flex-1">
           <Map
-            // initialViewState={{
-            //   longitude: parseFloat(markerLng ?? '0'),
-            //   latitude: parseFloat(markerLat ?? '0'),
-            //   zoom: 14,
-            // }}
+            ref={mapRef}
             style={{ width: '100%', height: '100%' }}
             mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN}
             mapStyle={process.env.NEXT_PUBLIC_MAPBOX_LIGHT_STYLE}
+            onLoad={fitBranches}
           >
-            {filteredBranches.map(({ latitude, longitude }) =>
+            {filteredBranches.map(({ latitude, longitude, slug }) =>
               latitude && longitude ? (
                 <Marker
                   // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
@@ -119,18 +182,34 @@ const MapSection = ({ ...rest }: MapSectionProps) => {
                   longitude={longitude}
                 >
                   <motion.button
-                    //   onClick={onMarkerClick}
                     style={{ originY: 1 }}
                     initial={{ scale: 0 }}
-                    animate={{ scale: 1 }}
+                    animate={{ scale: hoveredBranchSlug !== slug ? 0.75 : 1 }}
                     whileTap={{ scale: 0.8 }}
                   >
-                    <MapMarkerIcon />
+                    <MLink
+                      onMouseEnter={() => setHoveredBranchSlug(slug)}
+                      onMouseLeave={() => setHoveredBranchSlug(null)}
+                      noStyles
+                      href={`/branches/${slug}`}
+                    >
+                      <MapMarkerIcon />
+                    </MLink>
                   </motion.button>
                 </Marker>
               ) : null,
             )}
           </Map>
+        </div>
+        {/* mobile bottom button */}
+        <div className="absolute bottom-6 z-20 md:hidden">
+          <Button
+            className="rounded-full shadow"
+            onPress={() => setMapOrFiltersDisplayed((m) => !m)}
+            startIcon={<PlaceIcon />}
+          >
+            {isMapOrFiltersDisplayed ? t('filtering') : t('map')}
+          </Button>
         </div>
       </div>
     </Section>
