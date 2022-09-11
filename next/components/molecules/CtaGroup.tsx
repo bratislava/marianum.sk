3f8a3e2f91debb8cc5797f6ba3756e69dbffd0c5
Marek Bodinger <marek.bodinger@bratislava.sk>
2022-09-11 22:38:46 +0200

    Add key to map in CtaGroup.tsx

--- next/components/molecules/CtaGroup.tsx
+++ next/components/molecules/CtaGroup.tsx
@@ -7,8 +7,9 @@ const CtaGroup = ({ ctas }: CtaSectionFragment) => {
 
   return (
     <div className="grid auto-cols-fr gap-4 md:grid-flow-col">
-      {filteredCtas?.map(({ title, description, button }) => (
-        <div className="bg-primary px-4 py-8 text-white md:p-12">
+      {filteredCtas?.map(({ title, description, button }, index) => (
+        // eslint-disable-next-line react/no-array-index-key
+        <div className="bg-primary px-4 py-8 text-white md:p-12" key={index}>
           <div className="text-h3 font-bold">{title}</div>
           {description && <p className="mt-4 opacity-72">{description}</p>}
           {button && (
