ef31997ff0245be9ef57705842a0c97e0301d84c
Matej Hospodár <matej.hospodar@bratislava.sk>
2022-09-12 13:16:18 +0200

    create search page

---
+++ next/pages/search.tsx
@@ -0,0 +1,44 @@
+/* eslint-disable sonarjs/no-duplicate-string */
+import { GetStaticProps, GetStaticPropsResult } from 'next'
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+
+import PageWrapper from '../components/layouts/PageWrapper'
+import { GeneralEntityFragment, NavigationItemFragment } from '../graphql'
+import { client } from '../utils/gql'
+
+type HomeProps = {
+  navigation: NavigationItemFragment[]
+  general: GeneralEntityFragment | null
+}
+
+const Home = ({ navigation, general }: HomeProps) => {
+  return (
+    <PageWrapper navigation={navigation} general={general}>
+      <div>
+        <h1>Výsledky vyhľadávania</h1>
+      </div>
+    </PageWrapper>
+  )
+}
+
+export const getStaticProps: GetStaticProps = async ({
+  locale = 'sk',
+}): Promise<GetStaticPropsResult<HomeProps>> => {
+  const [{ navigation, general }, translations] = await Promise.all([
+    client.General({ locale }),
+    serverSideTranslations(locale, ['common']),
+  ])
+
+  const filteredNavigation = navigation.filter(Boolean) as NavigationItemFragment[]
+
+  return {
+    props: {
+      navigation: filteredNavigation,
+      general: general?.data ?? null,
+      ...translations,
+    },
+    revalidate: 10,
+  }
+}
+
+export default Home
