f6a57ee00ab717be8e8e99767a6ee2e6c91ee5f4
Matej Hospodár <matej.hospodar@bratislava.sk>
2022-09-13 13:36:24 +0200

    random length of paragraph

--- next/components/molecules/Row/RowSkeleton.tsx
+++ next/components/molecules/Row/RowSkeleton.tsx
@@ -1,6 +1,8 @@
+import { useMemo } from 'react'
 import Skeleton from 'react-loading-skeleton'
 
 const RowSkeleton = () => {
+  const width = useMemo(() => Math.random() * 300 + 100, [])
   return (
     <div className="flex h-[92px] flex-col justify-center gap-2 border border-border bg-white px-5">
       <div className="flex items-center gap-3">
@@ -8,7 +10,7 @@ const RowSkeleton = () => {
         <Skeleton width={48} height={20} borderRadius={1000} />
       </div>
       <p>
-        <Skeleton width={300} />
+        <Skeleton width={width} />
       </p>
     </div>
   )
