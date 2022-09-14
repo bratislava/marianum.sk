2777052249bc030b9468da6d03ed67868ac10b0a
Matej Hospodár <matej.hospodar@bratislava.sk>
2022-09-14 10:27:58 +0200

    hide map section

--- next/pages/[...slug].tsx
+++ next/pages/[...slug].tsx
@@ -13,7 +13,7 @@ import BundleListingSection from '../components/sections/BundleListingSection'
 import CardSection from '../components/sections/CardSection'
 import ContactsSection from '../components/sections/ContactsSection'
 import ImageGallerySection from '../components/sections/ImageGallerySection'
-import MapSection from '../components/sections/MapSection/MapSection'
+// import MapSection from '../components/sections/MapSection/MapSection'
 import MenuListingSection from '../components/sections/MenuListingSection'
 import NewsListing from '../components/sections/NewsListing'
 import PartnersSection from '../components/sections/PartnersSection'
@@ -162,11 +162,12 @@ const Slug = ({ navigation, page, general }: PageProps) => {
           }
           if (section?.__typename === 'ComponentSectionsMapSection') {
             return (
-              <MapSection
-                key={`${section.__typename}-${section.id}`}
-                isContainer={isContainer}
-                {...section}
-              />
+              // <MapSection
+              //   key={`${section.__typename}-${section.id}`}
+              //   isContainer={isContainer}
+              //   {...section}
+              // />
+              <span>Map</span>
             )
           }
           return null
