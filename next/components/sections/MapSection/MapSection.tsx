c95df0a2f1d596c5027ea1551607486509ac2127
Matej Hospodár <matej.hospodar@bratislava.sk>
2022-10-03 10:26:44 +0200

    add cooperative gestures

--- next/components/sections/MapSection/MapSection.tsx
+++ next/components/sections/MapSection/MapSection.tsx
@@ -188,6 +188,7 @@ const MapSection = ({ ...rest }: MapSectionProps) => {
             mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN}
             mapStyle={process.env.NEXT_PUBLIC_MAPBOX_LIGHT_STYLE}
             onLoad={() => fitBranches()}
+            cooperativeGestures
           >
             {filteredBranches.map(({ latitude, longitude, slug }) =>
               latitude && longitude ? (
