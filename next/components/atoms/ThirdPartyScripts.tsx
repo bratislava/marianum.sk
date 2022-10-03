9e179d10e1356ca60d8243e2c5755429d8130398
Matej Hospodár <matej.hospodar@bratislava.sk>
2022-10-03 17:32:52 +0200

    add google analytics

--- next/components/atoms/ThirdPartyScripts.tsx
+++ next/components/atoms/ThirdPartyScripts.tsx
@@ -1,6 +1,20 @@
+import { useRouter } from 'next/router'
 import Script from 'next/script'
+import { useEffect } from 'react'
+
+import * as gtag from '../../utils/googleAnalytics'
 
 const ThirdPartyScripts = () => {
+  const router = useRouter()
+  useEffect(() => {
+    router.events.on('routeChangeComplete', gtag.pageview)
+    router.events.on('hashChangeComplete', gtag.pageview)
+    return () => {
+      router.events.off('routeChangeComplete', gtag.pageview)
+      router.events.off('hashChangeComplete', gtag.pageview)
+    }
+  }, [router.events])
+
   return (
     <>
       {/* Plausible */}
@@ -8,6 +22,29 @@ const ThirdPartyScripts = () => {
         strategy="afterInteractive"
         data-domain={process.env.NEXT_PUBLIC_PLAUSIBLE_DOMAIN}
         src="https://plausible.io/js/plausible.js"
+      />
+
+      {/* Google Analytics */}
+      <Script
+        async
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${
+          process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID ?? ''
+        }`}
+      />
+      <Script
+        id="google-analytics-script"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID ?? ''}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
       />
     </>
   )
