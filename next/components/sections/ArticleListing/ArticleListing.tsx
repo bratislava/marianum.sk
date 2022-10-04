df72451bf9e0b12ca5e94bff745d112aa0eca673
Marek Bodinger <marek.bodinger@bratislava.sk>
2022-10-04 14:33:59 +0200

    Rename NewsSection to ArticleListing and support for tye

--- next/components/sections/ArticleListing/ArticleListing.tsx
+++ next/components/sections/ArticleListing/ArticleListing.tsx
@@ -1,22 +1,29 @@
 import { SearchResponse } from 'meilisearch'
 import { useTranslation } from 'next-i18next'
-import { useEffect, useState } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 import useSwr from 'swr'
 import { useDebounce } from 'usehooks-ts'
 
 import SearchIcon from '../../../assets/search.svg'
-import { Article, ArticleNewsCategory } from '../../../graphql'
-import { isDefined } from '../../../utils/isDefined'
+import {
+  Article,
+  ArticleListingFragment,
+  ArticleNewsCategory,
+  ArticlePressCategory,
+  Enum_Componentsectionsarticlelisting_Type,
+} from '../../../graphql'
 import { meiliClient } from '../../../utils/meilisearch'
 import useGetSwrExtras from '../../../utils/useGetSwrExtras'
 import Pagination from '../../atoms/Pagination/Pagination'
 import TextField from '../../atoms/TextField'
 import ArticleCard from '../../molecules/Cards/ArticleCard'
 import Section from '../../molecules/Section'
-import NewsSectionCategorySelect from './NewsSectionCategorySelect'
+import ArticleNewsCategoriesSelect from './ArticleNewsCategoriesSelect'
+import ArticlePressCategoriesSelect from './ArticlePressCategoriesSelect'
 
-type ArticleMeili = Omit<Article, '__typename' | 'newsCategory'> & {
-  newsCategory: Omit<ArticleNewsCategory, '__typename' | 'articles'>
+type ArticleMeili = Omit<Article, '__typename' | 'newsCategory' | 'pressCategory'> & {
+  newsCategory?: Omit<ArticleNewsCategory, '__typename' | 'articles'>
+  pressCategory?: Omit<ArticlePressCategory, '__typename' | 'articles'>
 }
 
 const pageSize = 24
@@ -29,7 +36,7 @@ type Filters = {
 
 const Articles = ({ data }: { data: SearchResponse<ArticleMeili> }) => {
   const { t } = useTranslation('common', {
-    keyPrefix: 'components.NewsSection',
+    keyPrefix: 'components.ArticleListing',
   })
 
   if (data.hits?.length > 0) {
@@ -60,16 +67,37 @@ const DataWrapper = ({
   filters,
   description,
   onPageChange,
+  section,
 }: {
   filters: Filters
   description?: string | null
   onPageChange: (page: number) => void
+  section: ArticleListingFragment
 }) => {
-  const { data, error } = useSwr(['NewsSection', filters], () => {
+  const sectionFilter = useMemo(() => {
+    switch (section.type) {
+      case Enum_Componentsectionsarticlelisting_Type.Press:
+        return filters.categoryId
+          ? `pressCategory.id = ${filters.categoryId}`
+          : // TODO: hacky solution, after update to Meilisearch 0.29 use "pressCategory EXISTS"
+            'pressCategory.id > 0'
+
+      case Enum_Componentsectionsarticlelisting_Type.News:
+        return filters.categoryId
+          ? `newsCategory.id = ${filters.categoryId}`
+          : // TODO: hacky solution, after update to Meilisearch 0.29 use "newsCategory EXISTS"
+            'newsCategory.id > 0'
+
+      default:
+        return null
+    }
+  }, [section.type, filters.categoryId])
+
+  const { data, error } = useSwr(['ArticleListing', filters, section.type], () => {
     return meiliClient.index('article').search<ArticleMeili>(filters.search, {
       limit: pageSize,
       offset: (filters.page - 1) * pageSize,
-      filter: isDefined(filters.categoryId) ? [`newsCategory.id = ${filters.categoryId}`] : [],
+      ...(sectionFilter ? { filter: sectionFilter } : {}),
       sort: ['updatedAtTimestamp:desc'],
     })
   })
@@ -108,8 +136,12 @@ const DataWrapper = ({
   )
 }
 
+type ArticleListingProps = {
+  section: ArticleListingFragment
+}
+
 // TODO: Overlap with header
-const NewsSection = () => {
+const ArticleListing = ({ section }: ArticleListingProps) => {
   const { t } = useTranslation()
 
   const [filters, setFilters] = useState<Filters>({
@@ -137,7 +169,12 @@ const NewsSection = () => {
     <Section>
       <div className="mb-4 grid grid-cols-1 gap-4 md:mb-6 md:grid-cols-3 md:bg-white md:p-6">
         <div>
-          <NewsSectionCategorySelect onCategoryChange={handleCategoryChange} />
+          {section.type === Enum_Componentsectionsarticlelisting_Type.Press ? (
+            <ArticlePressCategoriesSelect onCategoryChange={handleCategoryChange} />
+          ) : null}
+          {section.type === Enum_Componentsectionsarticlelisting_Type.News ? (
+            <ArticleNewsCategoriesSelect onCategoryChange={handleCategoryChange} />
+          ) : null}
         </div>
         <div className="md:col-span-2">
           <TextField
@@ -155,10 +192,10 @@ const NewsSection = () => {
       </div>
 
       <div>
-        <DataWrapper filters={filters} onPageChange={handlePageChange} />
+        <DataWrapper filters={filters} onPageChange={handlePageChange} section={section} />
       </div>
     </Section>
   )
 }
 
-export default NewsSection
+export default ArticleListing
