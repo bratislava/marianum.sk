f267156294caac27d6247a68e6e36ec04e1a3330
Matej Hospodár <matej.hospodar@bratislava.sk>
2022-10-07 10:24:29 +0200

    MD-152 Google analytics and Hotjar setup (#113)
    * add google analytics
    
    * add google-analytics to sentry blacklist
    
    * add hotjar
    
    Co-authored-by: Radoslav Zeman <19418224+dentalux@users.noreply.github.com>

--- next/components/atoms/ThirdPartyScripts.tsx
+++ next/components/atoms/ThirdPartyScripts.tsx
@@ -1,6 +1,20 @@
+import { useRouter } from 'next/router'
 import Script from 'next/script'
+import { useEffect } from 'react'
+
+import * as gtag from '../../utils/googleAnalytics'
 
 const ThirdPartyScripts = () => {
+  const router = useRouter()
+  useEffect(() => {
+    router.events.on('routeChangeComplete', gtag.pageview)
+    router.events.on('hashChangeComplete', gtag.pageview)
+    return () => {
+      router.events.off('routeChangeComplete', gtag.pageview)
+      router.events.off('hashChangeComplete', gtag.pageview)
+    }
+  }, [router.events])
+
   return (
     <>
       {/* Plausible */}
@@ -8,6 +22,47 @@ const ThirdPartyScripts = () => {
         strategy="afterInteractive"
         data-domain={process.env.NEXT_PUBLIC_PLAUSIBLE_DOMAIN}
         src="https://plausible.io/js/plausible.js"
+      />
+
+      {/* Google Analytics */}
+      <Script
+        async
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${
+          process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID ?? ''
+        }`}
+      />
+      <Script
+        id="google-analytics-script"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID ?? ''}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+
+      {/* Hotjar */}
+      <Script
+        id="hotjar-script"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(h,o,t,j,a,r){
+              h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+              h._hjSettings={hjid:'${process.env.NEXT_PUBLIC_HOTJAR_SITE_ID ?? ''}',hjsv:6};
+              a=o.getElementsByTagName('head')[0];
+              r=o.createElement('script');r.async=1;
+              r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+              a.appendChild(r);
+            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+          `,
+        }}
       />
     </>
   )
