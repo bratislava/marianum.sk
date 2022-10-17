05e38b4ff744ac7e3f43fcba19d2713196dff82d
Matej Hospodár <matej.hospodar@bratislava.sk>
2022-10-17 10:16:17 +0200

    replace ternary with regular if statements

--- next/components/sections/CardSection.tsx
+++ next/components/sections/CardSection.tsx
@@ -26,48 +26,56 @@ const CardSection = ({ section, ...rest }: CardSectionProps) => {
   const breakpoint = useTailwindBreakpoint()
   const isMobile = useMemo(() => breakpoint === null || breakpoint === 'sm', [breakpoint])
 
-  return style === Enum_Componentsectionsmanuallisting_Style.Simple ? (
-    <Section title={title} {...rest} cardGrid="cards" button={showMoreButton}>
-      {filteredPages?.map((page) => {
-        const { id, attributes } = page ?? {}
-        const { title: cardTitle } = attributes ?? {}
-        const fullPath = getFullSlug(page) ?? ''
+  if (style === Enum_Componentsectionsmanuallisting_Style.Simple) {
+    return (
+      <Section title={title} {...rest} cardGrid="cards" button={showMoreButton}>
+        {filteredPages?.map((page) => {
+          const { id, attributes } = page ?? {}
+          const { title: cardTitle } = attributes ?? {}
+          const fullPath = getFullSlug(page) ?? ''
 
-        return <CategoryCard key={id} title={cardTitle ?? ''} linkHref={fullPath} border />
-      })}
-    </Section>
-  ) : style === Enum_Componentsectionsmanuallisting_Style.Service ? (
-    <Section
-      title={title}
-      childrenWrapperClassName={cx({
-        'flex w-full gap-4 overflow-x-auto': isMobile,
-      })}
-      cardGrid={isMobile ? undefined : 'cards'}
-      {...rest}
-      button={showMoreButton}
-    >
-      {filteredPages?.map((page) => {
-        const { id, attributes } = page ?? {}
-        const { title: cardTitle, coverMedia, perex } = attributes ?? {}
+          return <CategoryCard key={id} title={cardTitle ?? ''} linkHref={fullPath} border />
+        })}
+      </Section>
+    )
+  }
 
-        const fullPath = getFullSlug(page) ?? ''
+  if (style === Enum_Componentsectionsmanuallisting_Style.Service) {
+    return (
+      <Section
+        title={title}
+        childrenWrapperClassName={cx({
+          'flex w-full gap-4 overflow-x-auto': isMobile,
+        })}
+        cardGrid={isMobile ? undefined : 'cards'}
+        {...rest}
+        button={showMoreButton}
+      >
+        {filteredPages?.map((page) => {
+          const { id, attributes } = page ?? {}
+          const { title: cardTitle, coverMedia, perex } = attributes ?? {}
 
-        return (
-          <ServiceCard
-            className={cx({
-              'w-[calc(100vw-6rem)] shrink-0 sm:w-[calc(100vw-16rem)]': isMobile,
-            })}
-            key={id}
-            title={cardTitle ?? ''}
-            linkHref={fullPath}
-            border
-            image={coverMedia?.data?.attributes}
-            subtitle={perex}
-          />
-        )
-      })}
-    </Section>
-  ) : null
+          const fullPath = getFullSlug(page) ?? ''
+
+          return (
+            <ServiceCard
+              className={cx({
+                'w-[calc(100vw-6rem)] shrink-0 sm:w-[calc(100vw-16rem)]': isMobile,
+              })}
+              key={id}
+              title={cardTitle ?? ''}
+              linkHref={fullPath}
+              border
+              image={coverMedia?.data?.attributes}
+              subtitle={perex}
+            />
+          )
+        })}
+      </Section>
+    )
+  }
+
+  return null
 }
 
 export default CardSection
