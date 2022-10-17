b6ae0ebeb04e79f2fce8349b11bcc44b10e51213
Radoslav Zeman <19418224+dentalux@users.noreply.github.com>
2022-10-17 16:34:14 +0200

    add non breaking space before specific symbols in NormalizeSkText (#140)

--- next/utils/normalizeSkText.ts
+++ next/utils/normalizeSkText.ts
@@ -1,12 +1,22 @@
 // https://github.com/LabZoneSK/predlozky/blob/main/src/index.js
-const prepositions = ['k', 's', 'v', 'z', 'o', 'u', 'na', 'za', 'do', 'a', 'i']
+// https://jazykovaporadna.sme.sk/q/3818/
+const prepositions = ['k', 's', 'v', 'z', 'o', 'u', 'a', 'i']
+// only used symbols are listed
+const symbols = ['€', '%']
 
 export const normalizeSkText = (text: string) => {
   let normalized = text
+
   prepositions.forEach((preposition) => {
     // eslint-disable-next-line security/detect-non-literal-regexp
     const re = new RegExp(` [${preposition}] `, 'gmi')
     normalized = normalized.replace(re, ` ${preposition}${String.fromCodePoint(160)}`)
+  })
+
+  symbols.forEach((symbol) => {
+    // eslint-disable-next-line security/detect-non-literal-regexp
+    const re = new RegExp(` [${symbol}]`, 'gmi')
+    normalized = normalized.replace(re, `${String.fromCodePoint(160)}${symbol}`)
   })
 
   return normalized
