cf7202edaa7872f2238c857e527a182ace1387cb
Marek Bodinger <marek.bodinger@bratislava.sk>
2022-10-18 22:15:39 +0200

    Extract loading

--- next/components/atoms/LoadingOverlay.tsx
+++ next/components/atoms/LoadingOverlay.tsx
@@ -1,9 +1,8 @@
 import cx from 'classnames'
 import { AnimatePresence, motion } from 'framer-motion'
-import { useTranslation } from 'next-i18next'
 import React, { PropsWithChildren } from 'react'
 
-import Spinner from './Spinner'
+import Loading from './Loading'
 
 type LoadingOverlayProps = { loading: boolean }
 
@@ -15,24 +14,19 @@ type LoadingOverlayProps = { loading: boolean }
 const ShrinkablePadding = () => <div className="h-20 min-h-[50%-44] shrink" />
 
 const LoadingOverlay = ({ loading, children }: PropsWithChildren<LoadingOverlayProps>) => {
-  const { t } = useTranslation()
-
   return (
     // 88px - height of spinner + loading text
     <div className={cx('relative', { 'min-h-[88]': loading })}>
       {loading ? (
         <AnimatePresence>
           <motion.div
-            className="absolute top-0 left-0 flex h-full w-full flex-col items-center bg-primary-dark/50 text-white"
+            className="absolute top-0 left-0 flex h-full w-full flex-col items-center bg-primary-dark/50"
             transition={{ duration: 0.2 }}
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
           >
             <ShrinkablePadding />
-            <div className="flex shrink-0 flex-col items-center gap-4">
-              <Spinner className="h-12 w-12" />
-              <div className="shrink-0">{t('components.atoms.LoadingOverlay.loading')}</div>
-            </div>
+            <Loading className="shrink-0 text-white" />
             <ShrinkablePadding />
           </motion.div>
         </AnimatePresence>
