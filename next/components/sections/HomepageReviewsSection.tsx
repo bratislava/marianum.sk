16a389134716f850aeee0fc85e7bbefb272bfb36
Matej Hospodár <matej.hospodar@bratislava.sk>
2022-10-18 13:58:29 +0200

    autoplay fixed, overflow removed

--- next/components/sections/HomepageReviewsSection.tsx
+++ next/components/sections/HomepageReviewsSection.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react'
+import { Autoplay } from 'swiper'
 import { Swiper, SwiperSlide } from 'swiper/react'
 
 import { ReviewEntityFragment } from '../../graphql'
@@ -16,41 +17,42 @@ const HomepageReviewsSection = ({ reviews, ...rest }: HomepageReviewsSectionProp
   }, [])
 
   return (
-    <div className="overflow-hidden">
-      <Section {...rest}>
-        {isBrowser && (
-          <Swiper
-            className="!overflow-visible"
-            spaceBetween={16}
-            loop
-            autoplay
-            breakpoints={{
-              640: {
-                slidesPerView: 2,
-              },
-              768: {
-                slidesPerView: 3,
-              },
-              1024: {
-                slidesPerView: 4,
-              },
-            }}
-          >
-            {reviews?.map((review, index) => (
-              // eslint-disable-next-line react/no-array-index-key, @typescript-eslint/restrict-template-expressions
-              <SwiperSlide key={`${review.id}-${index}`}>
-                <ReviewCard
-                  author={review.attributes?.author ?? ''}
-                  date={new Date(review.attributes?.date)}
-                  rating={review.attributes?.rating ?? 5}
-                  description={review.attributes?.description ?? ''}
-                />
-              </SwiperSlide>
-            ))}
-          </Swiper>
-        )}
-      </Section>
-    </div>
+    <Section {...rest}>
+      {isBrowser && (
+        <Swiper
+          spaceBetween={16}
+          loop
+          autoplay={{
+            delay: 3000,
+            disableOnInteraction: false,
+          }}
+          breakpoints={{
+            640: {
+              slidesPerView: 2,
+            },
+            768: {
+              slidesPerView: 3,
+            },
+            1024: {
+              slidesPerView: 4,
+            },
+          }}
+          modules={[Autoplay]}
+        >
+          {reviews?.map((review, index) => (
+            // eslint-disable-next-line react/no-array-index-key, @typescript-eslint/restrict-template-expressions
+            <SwiperSlide key={`${review.id}-${index}`}>
+              <ReviewCard
+                author={review.attributes?.author ?? ''}
+                date={new Date(review.attributes?.date)}
+                rating={review.attributes?.rating ?? 5}
+                description={review.attributes?.description ?? ''}
+              />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      )}
+    </Section>
   )
 }
 
