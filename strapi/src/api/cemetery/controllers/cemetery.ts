0ea8ac28b5c4a566a97e124a72cd9fdb5e778940
Radoslav Zeman <radoslav.zeman@bratislava.sk>
2022-11-01 13:24:53 +0100

    update strapi model and queries

---
+++ strapi/src/api/cemetery/controllers/cemetery.ts
@@ -0,0 +1,7 @@
+/**
+ * cemetery controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::cemetery.cemetery');
---
+++ strapi/src/api/cemetery/routes/cemetery.ts
@@ -0,0 +1,7 @@
+/**
+ * cemetery router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::cemetery.cemetery');
