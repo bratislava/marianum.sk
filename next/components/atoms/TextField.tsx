a4f10beb00fc458f8c677f14a16654f6dbfb5a15
Marek Bodinger <marek.bodinger@bratislava.sk>
2022-11-10 16:01:55 +0100

    forwardRef TextField

--- next/components/atoms/TextField.tsx
+++ next/components/atoms/TextField.tsx
@@ -1,5 +1,5 @@
 import cx from 'classnames'
-import { DetailedHTMLProps, InputHTMLAttributes, ReactNode, useId } from 'react'
+import { DetailedHTMLProps, forwardRef, InputHTMLAttributes, ReactNode, useId } from 'react'
 
 import FieldWrapper from './FieldWrapper'
 
@@ -27,20 +27,66 @@ type CommonProps = {
 
 type TextFieldProps = CommonProps & AreaOrInputConditionalProps
 
-const TextField = (props: TextFieldProps) => {
-  const generatedId = useId()
-  // eslint-disable-next-line react/destructuring-assignment
-  const generatedOrProvidedId = props.id ?? generatedId
+const TextField = forwardRef<HTMLTextAreaElement & HTMLInputElement, TextFieldProps>(
+  (props, ref) => {
+    const generatedId = useId()
+    // eslint-disable-next-line react/destructuring-assignment
+    const generatedOrProvidedId = props.id ?? generatedId
+    // eslint-disable-next-line react/destructuring-assignment
+    if (props.area) {
+      const {
+        area,
+        rows = 6,
+        label,
+        className,
+        inputClassName,
+        disabled = false,
+        error = false,
+        required = false,
+        isLarge = false,
+        ...rest
+      } = props
 
-  // eslint-disable-next-line react/destructuring-assignment
-  if (props.area) {
+      return (
+        <FieldWrapper
+          id={generatedOrProvidedId}
+          label={label}
+          className={className}
+          disabled={disabled}
+          error={error}
+          required={required}
+        >
+          <textarea
+            {...rest}
+            id={generatedOrProvidedId}
+            rows={rows}
+            disabled={disabled}
+            required={required}
+            className={cx(
+              'min-h-10 w-full resize-y bg-transparent px-4 py-[6px] outline-none',
+              inputClassName,
+              {
+                'text-foreground-disabled': disabled,
+                'placeholder:text-foreground-placeholder':
+                  !inputClassName?.includes('placeholder:'),
+                'h-16': isLarge,
+              },
+            )}
+            ref={ref}
+          />
+        </FieldWrapper>
+      )
+    }
+
     const {
+      id,
       area,
-      rows = 6,
+      leftSlot = null,
+      rightSlot = null,
       label,
       className,
-      inputClassName,
       disabled = false,
+      inputClassName,
       error = false,
       required = false,
       isLarge = false,
@@ -52,76 +98,36 @@ const TextField = (props: TextFieldProps) => {
         id={generatedOrProvidedId}
         label={label}
         className={className}
+        hasLeftSlot={!!leftSlot}
+        hasRightSlot={!!rightSlot}
         disabled={disabled}
         error={error}
         required={required}
       >
-        <textarea
+        {leftSlot && (
+          <div className={cx('shrink-0 grow-0', { 'p-3': isLarge, 'p-2': !isLarge })}>
+            {leftSlot}
+          </div>
+        )}
+        <input
           {...rest}
           id={generatedOrProvidedId}
-          rows={rows}
           disabled={disabled}
           required={required}
-          className={cx(
-            'min-h-10 w-full resize-y bg-transparent px-4 py-[6px] outline-none',
-            inputClassName,
-            {
-              'text-foreground-disabled': disabled,
-              'placeholder:text-foreground-placeholder': !inputClassName?.includes('placeholder:'),
-              'h-16': isLarge,
-            },
-          )}
+          className={cx('w-full bg-transparent outline-none', inputClassName, {
+            'text-foreground-disabled': disabled,
+            'pl-4': !leftSlot,
+            'pr-4': !rightSlot,
+            'placeholder:text-foreground-placeholder': !inputClassName?.includes('placeholder:'),
+            'h-16': isLarge,
+            'h-10': !isLarge,
+          })}
+          ref={ref}
         />
+        {rightSlot && <div className={cx('shrink-0 grow-0', { 'p-3': isLarge })}>{rightSlot}</div>}
       </FieldWrapper>
     )
-  }
-
-  const {
-    id,
-    area,
-    leftSlot = null,
-    rightSlot = null,
-    label,
-    className,
-    disabled = false,
-    inputClassName,
-    error = false,
-    required = false,
-    isLarge = false,
-    ...rest
-  } = props
-
-  return (
-    <FieldWrapper
-      id={generatedOrProvidedId}
-      label={label}
-      className={className}
-      hasLeftSlot={!!leftSlot}
-      hasRightSlot={!!rightSlot}
-      disabled={disabled}
-      error={error}
-      required={required}
-    >
-      {leftSlot && (
-        <div className={cx('shrink-0 grow-0', { 'p-3': isLarge, 'p-2': !isLarge })}>{leftSlot}</div>
-      )}
-      <input
-        {...rest}
-        id={generatedOrProvidedId}
-        disabled={disabled}
-        required={required}
-        className={cx('w-full bg-transparent outline-none', inputClassName, {
-          'text-foreground-disabled': disabled,
-          'pl-4': !leftSlot,
-          'pr-4': !rightSlot,
-          'placeholder:text-foreground-placeholder': !inputClassName?.includes('placeholder:'),
-          'h-16': isLarge,
-          'h-10': !isLarge,
-        })}
-      />
-      {rightSlot && <div className={cx('shrink-0 grow-0', { 'p-3': isLarge })}>{rightSlot}</div>}
-    </FieldWrapper>
-  )
-}
+  },
+)
 
 export default TextField
