ce4a10e5b6e05e398b19f369a9ea4f9772b6bbf2
MarekBodingerBA <104828482+MarekBodingerBA@users.noreply.github.com>
2022-11-23 15:54:54 +0100

    Image temp fix (#220)
    * Images fix
    
    * Images fix v2
    
    * Images fix v3

--- next/components/atoms/MImage.tsx
+++ next/components/atoms/MImage.tsx
@@ -5,7 +5,14 @@ import { ComponentProps } from 'react'
 // After update to Next.js, loading images is broken (returns 503 and makes the server crash for a moment), this is a
 // temporary fix.
 export const customImageLoader: ImageLoader = ({ src, width, quality }) => {
-  return `https://www.mestskakniznica.sk/_next/image?url=${src}&w=${width}&q=${quality || 75}`
+  // https://sourcegraph.com/github.com/vercel/next.js@33d4694ba7a3847464b32d33229fd88cadadd64c/-/blob/packages/next/client/legacy/image.tsx?L168
+  if (src.endsWith('.svg')) {
+    return src
+  }
+
+  return `https://www.mestskakniznica.sk/_next/image?url=${encodeURIComponent(src)}&w=${width}&q=${
+    quality || 75
+  }`
 }
 
 export type MImageImage = Pick<
