062fc257c88e28280cd7a78e8fe09555baf290e5
Radoslav Zeman <radoslav.zeman@bratislava.sk>
2022-11-30 12:07:14 +0100

    dummy commit to fresh rebuild 2

--- next/components/atoms/Button.tsx
+++ next/components/atoms/Button.tsx
@@ -31,7 +31,6 @@ export type ButtonProps = Omit<AriaButtonProps<'button'>, keyof LinkButtonProps>
     ref?: Ref<HTMLButtonElement>
     href?: undefined
   }
-
 export type AnchorProps = AriaButtonProps<'a'> &
   ButtonBase & {
     ref?: Ref<HTMLAnchorElement>
