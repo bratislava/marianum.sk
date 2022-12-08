da3ad41823e1a572cc25fe686ac8592dd692cd42
Marek Bodinger <marek.bodinger@bratislava.sk>
2022-12-08 14:02:38 +0100

    Fix types

--- next/components/atoms/Forms/FormTextField.tsx
+++ next/components/atoms/Forms/FormTextField.tsx
@@ -1,15 +1,18 @@
 import FormErrorWrapper from '@components/atoms/Forms/FormErrorWrapper'
 import TextField from '@components/atoms/TextField'
 import React, { ComponentProps, forwardRef, PropsWithChildren } from 'react'
-import { FormState } from 'react-hook-form'
+import { FieldValues, FormState } from 'react-hook-form'
 
 // Types are not worth the effort.
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-type FormTextFieldProps<T = any> = ComponentProps<typeof TextField> & { formState: FormState<T> }
+type FormTextFieldProps<T extends FieldValues> = ComponentProps<typeof TextField> & {
+  formState: FormState<T>
+}
 
 const FormTextField = forwardRef<
   HTMLTextAreaElement & HTMLInputElement,
-  PropsWithChildren<FormTextFieldProps>
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  PropsWithChildren<FormTextFieldProps<any>>
 >(({ children, formState, ...props }, ref) => {
   return (
     <FormErrorWrapper formState={formState} name={props.name}>
