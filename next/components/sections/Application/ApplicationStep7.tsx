150bfc3ba7d0a29c7fca490c4013c06cdee32cce
Marek Bodinger <marek.bodinger@bratislava.sk>
2022-12-20 00:03:34 +0100

    Use new wrapper

--- next/components/sections/Application/ApplicationStep7.tsx
+++ next/components/sections/Application/ApplicationStep7.tsx
@@ -5,7 +5,7 @@ import { ApplicationStepComponentProps } from '@components/sections/Application/
 
 import { ApplicationTypes } from './application-shared.types'
 import { step7YupShape } from './application-shared.yup'
-import { useApplicationStep } from './useApplicationStep'
+import { ApplicationStepWrapper, useApplicationStep } from './useApplicationStep'
 
 const defaultValues: ApplicationTypes.Step7Model = {
   poznamka: '',
@@ -20,21 +20,23 @@ const ApplicationStep7 = ({
   onFormChange,
   sending,
 }: ApplicationStepComponentProps<ApplicationTypes.Step.Step7>) => {
-  const { register, control, formState, Wrapper } = useApplicationStep({
-    yupShape: step7YupShape,
-    values,
-    defaultValues,
-    onContinue,
-    onFormChange,
-    sendStep: true,
-    sending,
-  })
+  const { register, control, formState, handleSubmit } =
+    useApplicationStep<ApplicationTypes.Step7Model>({
+      yupShape: step7YupShape,
+      values,
+      defaultValues,
+      onFormChange,
+    })
 
   return (
-    <Wrapper>
+    <ApplicationStepWrapper
+      handleSubmit={handleSubmit}
+      onContinue={onContinue}
+      sending={sending}
+      sendStep
+    >
       <h3 className="mb-3 md:mb-6">Poznámka</h3>
       <TextField area {...register('poznamka')} />
-
       <h3 className="mt-8 mb-4">Súhlasy</h3>
       <div className="grid gap-4 pb-4 md:pb-6">
         <FormCheckbox name="suhlasSOdoslanim" control={control} formState={formState}>
@@ -60,7 +62,7 @@ const ApplicationStep7 = ({
           </MLink>
         </FormCheckbox>
       </div>
-    </Wrapper>
+    </ApplicationStepWrapper>
   )
 }
 
