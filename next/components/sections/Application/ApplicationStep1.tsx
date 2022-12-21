e2aeb9989eccc78247e327c2e132720abe17491a
Marek Bodinger <marek.bodinger@bratislava.sk>
2022-12-21 16:36:20 +0100

    Texts in application

--- next/components/sections/Application/ApplicationStep1.tsx
+++ next/components/sections/Application/ApplicationStep1.tsx
@@ -14,6 +14,7 @@ const ApplicationStep1 = ({
   values,
   onContinue,
   onFormChange,
+  texts,
 }: ApplicationStepComponentProps<ApplicationTypes.Step.Step1>) => {
   const { control, Wrapper, formState } = useApplicationStep({
     yupShape: step1YupShape,
@@ -32,10 +33,18 @@ const ApplicationStep1 = ({
         formState={formState}
         className="grid gap-4 pb-6 md:grid-cols-2 md:gap-6"
       >
-        <RadioBox value={ApplicationTypes.TypZiadosti.Pridelenie} className="grow">
+        <RadioBox
+          value={ApplicationTypes.TypZiadosti.Pridelenie}
+          className="grow"
+          tooltip={texts.typZiadostiPridelenieTooltip}
+        >
           Žiadam o pridelenie hrobového miesta
         </RadioBox>
-        <RadioBox value={ApplicationTypes.TypZiadosti.Rezervacia} className="grow">
+        <RadioBox
+          value={ApplicationTypes.TypZiadosti.Rezervacia}
+          className="grow"
+          tooltip={texts.typZiadostiRezervaciaTooltip}
+        >
           Žiadam o rezerváciu hrobového miesta
         </RadioBox>
       </FormRadioGroup>
