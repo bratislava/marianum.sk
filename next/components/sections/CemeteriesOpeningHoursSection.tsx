835e143740fbc0eca97c54e7f36d011fb4c5525c
Radoslav Zeman <19418224+radoslavzeman@users.noreply.github.com>
2023-01-15 21:18:19 +0100

    chenge cemetery opening hours to use component instead of richtext (#263)

---
+++ next/components/sections/CemeteriesOpeningHoursSection.tsx
@@ -0,0 +1,51 @@
+import Button from '@components/atoms/Button'
+import { sectionContext } from '@components/layouts/SectionsWrapper'
+import { useGetFullPath } from '@components/molecules/Navigation/NavigationProvider/useGetFullPath'
+import { useNavigationContext } from '@components/molecules/Navigation/NavigationProvider/useNavigationContext'
+import OpeningHours from '@components/molecules/OpeningHours'
+import Section, { SectionProps } from '@components/molecules/Section'
+import { CemeteriesOpeningHoursFragment } from '@graphql'
+import cx from 'classnames'
+import { useContext } from 'react'
+
+type CemeteriesOpeningHoursSectionProps = Pick<SectionProps, 'background' | 'title'> & {
+  section: CemeteriesOpeningHoursFragment
+}
+
+const CemeteriesOpeningHoursSection = ({
+  section,
+  ...rest
+}: CemeteriesOpeningHoursSectionProps) => {
+  const { general } = useNavigationContext()
+  const { border } = useContext(sectionContext)
+  const { getFullPath } = useGetFullPath()
+
+  const showMorePath = getFullPath(section.showMoreButton?.page?.data)
+
+  return (
+    <Section
+      title={section.title}
+      button={section.buttonPosition === 'standard' ? section.showMoreButton : undefined}
+      {...rest}
+    >
+      <div
+        className={cx('bg-white py-3 px-4 md:py-4 md:px-5', {
+          'border border-border': border,
+        })}
+      >
+        {general?.cemeteryOpeningHours && (
+          <OpeningHours openingHours={general?.cemeteryOpeningHours} />
+        )}
+      </div>
+      {showMorePath && section.buttonPosition === 'below' && (
+        <div className="mt-6 flex justify-center md:mt-8 md:justify-start">
+          <Button variant="primary" href={showMorePath}>
+            {section.showMoreButton?.label}
+          </Button>
+        </div>
+      )}
+    </Section>
+  )
+}
+
+export default CemeteriesOpeningHoursSection
