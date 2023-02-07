a20e597a4da1fc65125502dda6456f0994ef09cb
Radoslav Zeman <radoslav.zeman@bratislava.sk>
2023-02-07 12:59:58 +0100

    render NEXT_PUBLIC_ env vars into source code

--- next/components/layouts/PageWrapper.tsx
+++ next/components/layouts/PageWrapper.tsx
@@ -13,11 +13,43 @@ type PageWrapperProps = {
   general: GeneralEntityFragment | null
 }
 
+/* eslint-disable @typescript-eslint/restrict-template-expressions */
+const NextPublicEnvsHtmlComment = () => {
+  const headerHtmlComment = `
+<!-- 
+=== public env vars ===
+  NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN: ${process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN}
+  NEXT_PUBLIC_MAPBOX_DARK_STYLE: ${process.env.NEXT_PUBLIC_MAPBOX_DARK_STYLE}
+  NEXT_PUBLIC_MAPBOX_LIGHT_STYLE: ${process.env.NEXT_PUBLIC_MAPBOX_LIGHT_STYLE}
+  
+  NEXT_PUBLIC_MEILISEARCH_HOST: ${process.env.NEXT_PUBLIC_MEILISEARCH_HOST}
+  NEXT_PUBLIC_MEILISEARCH_SEARCH_API_KEY: ${process.env.NEXT_PUBLIC_MEILISEARCH_SEARCH_API_KEY}
+
+  NEXT_PUBLIC_PLAUSIBLE_DOMAIN: ${process.env.NEXT_PUBLIC_PLAUSIBLE_DOMAIN}
+  NEXT_PUBLIC_HOTJAR_SITE_ID: ${process.env.NEXT_PUBLIC_HOTJAR_SITE_ID}
+  
+  NEXT_PUBLIC_CLOUDFLARE_TURNSTILE_SITE_KEY: ${process.env.NEXT_PUBLIC_CLOUDFLARE_TURNSTILE_SITE_KEY}
+=======================
+-->
+`
+  return (
+    <div
+      id="next_public_env_vars"
+      // eslint-disable-next-line react/no-danger
+      dangerouslySetInnerHTML={{
+        __html: headerHtmlComment,
+      }}
+    />
+  )
+}
+/* eslint-enable @typescript-eslint/restrict-template-expressions */
+
 const PageWrapper = ({ navigation, header, children, general }: PageWrapperProps) => {
   return (
     <NavigationProvider navigation={navigation} general={general}>
       <div className="h-full">
         <header>
+          <NextPublicEnvsHtmlComment />
           <Navigation contact={general?.attributes?.header?.contact?.data} />
           {header}
         </header>
