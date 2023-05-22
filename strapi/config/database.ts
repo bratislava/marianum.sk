c61ff2a69d447f9a655434d88c21817546d5c31c
Radoslav Zeman <19418224+radoslavzeman@users.noreply.github.com>
2023-05-22 11:51:57 +0200

    upgrade strapi to v4.6.1

--- strapi/config/database.ts
+++ strapi/config/database.ts
@@ -1,13 +1,16 @@
 export default ({ env }) => ({
   connection: {
-    client: 'postgres',
+    client: "postgres",
+    // https://docs.strapi.io/dev-docs/configurations/database#connection-parameters
     connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
-      port: env.int('DATABASE_PORT', 54320),
-      database: env('DATABASE_NAME', 'strapi'),
-      user: env('DATABASE_USERNAME', 'postgres'),
-      password: env('DATABASE_PASSWORD', 'postgres'),
-      ssl: env.bool('DATABASE_SSL', false),
+      connectionString: "",
+      host: env("DATABASE_HOST", "localhost"),
+      port: env.int("DATABASE_PORT", 5432),
+      database: env("DATABASE_NAME", "strapi"),
+      user: env("DATABASE_USERNAME", "strapi"),
+      password: env("DATABASE_PASSWORD", "password"),
+      schema: env("DATABASE_SCHEMA", "public"),
     },
+    debug: false,
   },
 });
