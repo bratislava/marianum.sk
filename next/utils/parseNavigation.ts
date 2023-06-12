94a97e89d281bc74ca734494b73db32421ef7401
Marek Bodinger <marek.bodinger@bratislava.sk>
2023-06-12 18:32:31 +0200

    Fix parseNavigation

--- next/utils/parseNavigation.ts
+++ next/utils/parseNavigation.ts
@@ -1,18 +1,15 @@
 import { NavigationItemFragment } from '@graphql'
 import { isDefined } from '@utils/isDefined'
-import last from 'lodash/last'
 
 const navMap = new Map<string, { label: string; path: string }>()
 
 export type NavMap = typeof navMap
 
 export const parseNavigation = (navItems: NavigationItemFragment[]) => {
-  navItems.forEach(({ path, items, title }) => {
-    if (path) {
-      const slug = last(path?.split('/'))
-      if (slug) {
-        navMap.set(slug, { label: title, path })
-      }
+  navItems.forEach(({ path, items, title, related }) => {
+    const slug = related?.attributes?.slug
+    if (slug && path) {
+      navMap.set(slug, { label: title, path })
     }
     if (items) {
       parseNavigation(items.filter(isDefined))
