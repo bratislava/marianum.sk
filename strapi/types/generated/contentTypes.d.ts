65ccc60e2a6f3935a1a95c48b8c157d7ab27c91b
Jana Běhal Dadová <jana.dadova@bratislava.sk>
2025-03-03 14:41:56 +0100

    Strapi setup

--- strapi/types/generated/contentTypes.d.ts
+++ strapi/types/generated/contentTypes.d.ts
@@ -917,6 +917,12 @@ export interface ApiCemeteryCemetery extends Schema.CollectionType {
           localized: true;
         };
       }>;
+    documents: Attribute.Component<'sections.document-group'> &
+      Attribute.SetPluginOptions<{
+        i18n: {
+          localized: true;
+        };
+      }>;
     latitude: Attribute.Float &
       Attribute.SetPluginOptions<{
         i18n: {
