05541117eda4f325ca2d4346296182d8f4b40a29
Adam Jakubička <139510883+Ty-ci@users.noreply.github.com>
2025-03-10 16:03:49 +0100

    Fix graphql index.ts (#516)

--- strapi/types/generated/contentTypes.d.ts
+++ strapi/types/generated/contentTypes.d.ts
@@ -2347,6 +2347,11 @@ export interface PluginUploadFolder extends Schema.CollectionType {
     };
   };
   attributes: {
+    children: Attribute.Relation<
+      'plugin::upload.folder',
+      'oneToMany',
+      'plugin::upload.folder'
+    >;
     createdAt: Attribute.DateTime;
     createdBy: Attribute.Relation<
       'plugin::upload.folder',
@@ -2358,11 +2363,6 @@ export interface PluginUploadFolder extends Schema.CollectionType {
       'plugin::upload.folder',
       'oneToMany',
       'plugin::upload.file'
-    >;
-    children: Attribute.Relation<
-      'plugin::upload.folder',
-      'oneToMany',
-      'plugin::upload.folder'
     >;
     name: Attribute.String &
       Attribute.Required &
