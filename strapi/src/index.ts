000f58151e3b87d85bde9e330981f1a94aed7173
Jana Běhal Dadová <jana.dadova@bratislava.sk>
2025-03-24 15:15:45 +0100

    Boothstrapped Revalidate Webhook setup

--- strapi/src/index.ts
+++ strapi/src/index.ts
@@ -1,3 +1,5 @@
+import { Strapi } from '@strapi/strapi'
+
 export default {
   /**
    * An asynchronous register function that runs before
@@ -6,7 +8,7 @@ export default {
    * This gives you an opportunity to extend code.
    */
   register({ strapi }) {
-    const extensionService = strapi.service("plugin::graphql.extension");
+    const extensionService = strapi.service("plugin::graphql.extension")
     extensionService.use(({ strapi }) => ({
       typeDefs: `
             type Query {
@@ -26,7 +28,7 @@ export default {
           auth: false,
         },
       },
-    }));
+    }))
   },
 
   /**
@@ -36,5 +38,28 @@ export default {
    * This gives you an opportunity to set up your data model,
    * run jobs, or perform some special logic.
    */
-  async bootstrap(/*{ strapi }*/) {},
-};
+  async bootstrap({ strapi }: { strapi: Strapi }) {
+    console.log('Bootstrap function started')
+
+    // create Revalidate webhook according to this suggestion https://github.com/strapi/strapi/pull/20487#issuecomment-2482527848
+    const webhook = await strapi.db.query('webhook').findOne({
+      where: {
+        name: 'Bootstrapped Revalidate',
+      },
+    })
+
+    if (!webhook) {
+      await strapi.webhookStore.createWebhook({
+        id: 'Bootstrapped Revalidate',
+        name: 'Bootstrapped Revalidate',
+        url: `${process.env.REVALIDATE_NEXT_URL}/api/revalidate?secret=${process.env.REVALIDATE_SECRET_TOKEN}`,
+        events: ['entry.create', 'entry.update', 'entry.publish'],
+        headers: {},
+        isEnabled: true
+      })
+      console.log('Revalidate webhook created')
+    } else {
+      console.log('Revalidate webhook already exists')
+    }
+  },
+}
