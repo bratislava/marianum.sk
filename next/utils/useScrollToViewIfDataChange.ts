4f56ee6fea715a2dabeecb7cc0fcc6fb94c2656f
Radoslav Zeman <19418224+radoslavzeman@users.noreply.github.com>
2026-01-09 12:59:29 +0100

    Upgrade usehooks-ts to v3

--- next/utils/useScrollToViewIfDataChange.ts
+++ next/utils/useScrollToViewIfDataChange.ts
@@ -1,31 +1,24 @@
 import { RefObject, useEffect, useState } from 'react'
 import scrollIntoView from 'smooth-scroll-into-view-if-needed'
-import { useIsFirstRender, useIsomorphicLayoutEffect } from 'usehooks-ts'
+import { useIsomorphicLayoutEffect } from 'usehooks-ts'
 
 export const useScrollToViewIfDataChange = <T extends object | null, K extends object>(
   data: T | undefined,
   filters: K,
   elementRef: RefObject<HTMLElement>,
 ) => {
-  // We don't want to scroll to the content initially.
-  const isFirst = useIsFirstRender()
-
   // Relying on data change is not sufficient, first filters are changed and then data are asynchronously fetched.
   // Therefore, we mark filters as changed to scroll when needed. However, sometimes data change without any filter change
   // (e.g. when the library revalidates the data on focus), in this case we don't want to scroll to the content.
   const [filtersChanged, setFiltersChanged] = useState(false)
 
   useEffect(() => {
-    if (isFirst) {
-      return
-    }
-
     setFiltersChanged(true)
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [filters])
 
   useIsomorphicLayoutEffect(() => {
-    if (!elementRef?.current || isFirst || !filtersChanged) {
+    if (!elementRef?.current || !filtersChanged) {
       return
     }
 
