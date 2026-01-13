55dfb3c6bebf9257b4b057314738ea486ea66f56
Radoslav Zeman <19418224+radoslavzeman@users.noreply.github.com>
2026-01-13 07:59:26 +0100

    Remove serverRuntimeConfig and use NEXT_PUBLIC_ env instead

--- next/README.md
+++ next/README.md
@@ -36,4 +36,4 @@ Open [http://localhost:3000](http://localhost:3000) with your browser to see the
 
 ## Static Site Generation
 
-If you want to test static site generation locally, you need to run `yarn build` and `yarn start`. This commands run by default with the prod env variable, so in order to have the local env variable for strapi, you need to create `.env.local` with `STRAPI_URL=localhost:1337` to override the prod values. This file is ignored by git, because it often contains sensitive secrets
+If you want to test static site generation locally, you need to run `yarn build` and `yarn start`. This commands run by default with the prod env variable, so in order to have the local env variable for strapi, you need to create `.env.local` with `NEXT_PUBLIC_STRAPI_URL=localhost:1337` to override the prod values. This file is ignored by git, because it often contains sensitive secrets
--- next/services/graphql/gqlClient.ts
+++ next/services/graphql/gqlClient.ts
@@ -1,31 +1,5 @@
 import { getSdk } from '@/graphql'
 import { GraphQLClient } from 'graphql-request'
-import getConfig from 'next/config'
 
-function isServer() {
-  return typeof window === 'undefined'
-}
-
-const { serverRuntimeConfig } = getConfig()
-
-// URL becomes full url to strapi on server, but just /graphql (for proxy) on client
-const protocol =
-  serverRuntimeConfig?.strapiUrl &&
-  (serverRuntimeConfig?.strapiUrl.startsWith('http://') ||
-    serverRuntimeConfig?.strapiUrl.startsWith('https://'))
-    ? ''
-    : 'http://'
-
-const buildUrl = (path: string): string =>
-  `${
-    serverRuntimeConfig?.strapiUrl
-      ? // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
-        `${protocol}${serverRuntimeConfig.strapiUrl}`
-      : isServer()
-        ? ''
-        : window.location.origin
-  }${path}`
-
-const gql = new GraphQLClient(buildUrl('/graphql'))
-
+const gql = new GraphQLClient(`${process.env.NEXT_PUBLIC_NEXT_PUBLIC_STRAPI_URL}/graphql`)
 export const client = getSdk(gql)
