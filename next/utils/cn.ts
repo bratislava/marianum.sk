b758255b51c72153853bcb1cdddc7b9c9f736dff
Ty-ci <teachy.jaho@gmail.com>
2026-05-25 15:16:23 +0200

    Cleanup comments in cn

--- next/utils/cn.ts
+++ next/utils/cn.ts
@@ -1,8 +1,9 @@
-// cn helper function inspired by https://ui.shadcn.com/docs/installation/manual
 import { type ClassValue, clsx } from 'clsx'
 import { extendTailwindMerge } from 'tailwind-merge'
 
-// NEED TO CHANGE
+/**
+ * Inspired by: https://ui.shadcn.com/docs/installation/manual
+ */
 
 const twMerge = extendTailwindMerge({
   extend: {
