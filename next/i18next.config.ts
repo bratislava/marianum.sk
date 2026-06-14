9b40f8fa2bbd5beaa0f8e644739e03da12a12b03
Ty-ci <teachy.jaho@gmail.com>
2026-06-14 19:52:23 +0200

    Fix translations parser

--- next/i18next.config.ts
+++ next/i18next.config.ts
@@ -1,26 +1,17 @@
-import { defineConfig } from 'i18next-cli';
+import { defineConfig } from 'i18next-cli'
 
 export default defineConfig({
-  "locales": [
-    "sk"
-  ],
-  "extract": {
-    "input": "{assets,components,pages,services,styles,utils}/**/*.{tsx,ts}",
-    "output": "./public/locales/{{language}}/{{namespace}}.json",
-    "defaultNS": "translation",
-    "keySeparator": false,
-    "functions": [
-      "t",
-      "*.t"
-    ],
-    "transComponents": [
-      "Trans"
-    ]
+  locales: ['sk'],
+  extract: {
+    input: '{assets,components,pages,services,styles,utils}/**/*.{tsx,ts}',
+    output: './public/locales/{{language}}/{{namespace}}.json',
+    defaultNS: 'translation',
+    keySeparator: false,
+    functions: ['t', '*.t'],
+    transComponents: ['Trans'],
   },
-  "types": {
-    "input": [
-      "public/locales/{{language}}/{{namespace}}.json"
-    ],
-    "output": "src/types/i18next.d.ts"
-  }
-});
+  types: {
+    input: ['public/locales/{{language}}/{{namespace}}.json'],
+    output: 'src/types/i18next.d.ts',
+  },
+})
