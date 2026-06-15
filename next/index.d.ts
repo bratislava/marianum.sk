b8fed6b4a50cd09dd9e09a2e67d43f3e0094686d
Ty-ci <teachy.jaho@gmail.com>
2026-06-15 10:09:45 +0200

    Fix ts errors

---
+++ next/index.d.ts
@@ -0,0 +1,15 @@
+declare module '*.svg' {
+  import * as React from 'react'
+
+  const ReactComponent: React.FunctionComponent<
+    React.SVGProps<SVGSVGElement> & { title?: string; className?: string }
+  >
+
+  const src = ReactComponent
+  export default src
+}
+
+declare module '*.svg?url' {
+  const content: unknown
+  export default content
+}
--- next/services/meili/meiliClient.ts
+++ next/services/meili/meiliClient.ts
@@ -1,6 +1,6 @@
-import { Meilisearch } from 'meilisearch'
+import { MeiliSearch } from 'meilisearch'
 
-export const meiliClient = new Meilisearch({
+export const meiliClient = new MeiliSearch({
   host: process.env.NEXT_PUBLIC_MEILISEARCH_HOST ?? '',
   apiKey: process.env.NEXT_PUBLIC_MEILISEARCH_SEARCH_API_KEY,
 })
