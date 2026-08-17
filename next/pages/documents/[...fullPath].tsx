3169e34a3220ef3d7457ede57fa410fd6a3e8a09
Petr Šigut <64728606+shegood@users.noreply.github.com>
2026-08-17 12:26:34 +0200

    Update next/pages/documents/[...fullPath].tsx
    Co-authored-by: Adam Jakubička <139510883+Ty-ci@users.noreply.github.com>

--- next/pages/documents/[...fullPath].tsx
+++ next/pages/documents/[...fullPath].tsx
@@ -55,7 +55,7 @@ export const getStaticProps: GetStaticProps<DocumentPageProps, StaticParams> = a
   params,
 }) => {
   // eslint-disable-next-line no-console
-  console.log(`Revalidating asset ${params?.fullPath.join('/') ?? ''}`)
+  console.log(`Revalidating Asset ${params?.fullPath.join('/') ?? ''}`)
 
   return (
     // TODO: Locales
